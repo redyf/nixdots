a76ee0f797a7a94dbf4d920a1e04ce4017782488
Redyf <mateusalvespereira7@gmail.com>
2023-01-13 00:32:50 -0300

    Added new flake inputs for theming

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -16,8 +16,45 @@
   # changes in each release.
   home.stateVersion = "22.11";
 
-  # Overlays
+  # Imports
+    imports = [
+      nix-colors.homeManagerModule
+    ];
 
+  #   colorScheme = {
+  #   slug = "oxocarbon-dark";
+  #   name = "Oxocarbon-Dark";
+  #   author = "shaunsingh/IBM";
+  #   colors = {
+  #     base00 = "#161616";
+  #     base01 = "#262626";
+  #     base02 = "#393939";
+  #     base03 = "#525252";
+  #     base04 = "#dde1e6";
+  #     base05 = "#f2f4f8";
+  #     base06 = "#ffffff";
+  #     base07 = "#08bdba";
+  #     base08 = "#3ddbd9";
+  #     base09 = "#78a9ff";
+  #     base0A = "#ee5396";
+  #     base0B = "#33b1ff";
+  #     base0C = "#ff7eb6";
+  #     base0D = "#42be65";
+  #     base0E = "#be95ff"; 
+  #     base0F = "#82cfff";
+  #   };
+  # };
+
+# programs = {
+#     kitty = {
+#       enable = true;
+#       settings = {
+#         foreground = "#${config.colorScheme.colors.base05}";
+#         background = "#${config.colorScheme.colors.base00}";
+#         # ...
+#       };
+
+  # Overlays
   nixpkgs.overlays = [ 
   	(self: super: {
 		discord = super.discord.overrideAttrs (
@@ -28,7 +65,6 @@
 	})
   (import ~/flake/overlays/firefox-overlay.nix)
 ];
-
 
   # customNeovim = import ./config/nvim/nvim.nix;
   home.packages = with pkgs; [
