e8eb8482330b6d7e487b32d70999977c45834b0f
Redyf <mateusalvespereira7@gmail.com>
2023-05-11 18:08:20 -0300

    New zsh plugins + improved code + updated flakes

--- flake.nix
+++ flake.nix
@@ -9,44 +9,38 @@
     nur.url = "github:nix-community/NUR";
     waybar.url = "github:alexays/Waybar";
 
-    # SFMono w/ patches
-    sf-mono-liga-src = {
-      url = "github:shaunsingh/SFMono-Nerd-Font-Ligaturized";
-      flake = false;
-    };
-
     # Home manager
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    hyprland,
-    home-manager,
-    ...
-  } @ inputs: {
-    nixosConfigurations = {
-      redyf = nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs hyprland;};
-        modules = [
-          ./nixos/configuration.nix
-          hyprland.nixosModules.default
-          {programs.hyprland.enable = true;}
-        ];
+  outputs =
+    { self
+    , nixpkgs
+    , hyprland
+    , home-manager
+    , ...
+    } @ inputs: {
+      nixosConfigurations = {
+        redyf = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs hyprland; };
+          modules = [
+            ./nixos/configuration.nix
+            hyprland.nixosModules.default
+            { programs.hyprland.enable = true; }
+          ];
+        };
       };
-    };
 
-    # Standalone home-manager configuration entrypoint
-    # Available through 'home-manager --flake .#your-username@your-hostname'
-    homeConfigurations = {
-      "redyf@nixos" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
-        extraSpecialArgs = {inherit inputs;}; # Pass flake inputs to our config
-        # > Our main home-manager configuration file <
-        modules = [./home-manager/home.nix];
+      # Standalone home-manager configuration entrypoint
+      # Available through 'home-manager --flake .#your-username@your-hostname'
+      homeConfigurations = {
+        "redyf@nixos" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+          extraSpecialArgs = { inherit inputs; }; # Pass flake inputs to our config
+          # > Our main home-manager configuration file <
+          modules = [ ./home-manager/home.nix ];
+        };
       };
     };
-  };
 }
