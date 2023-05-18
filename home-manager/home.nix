b704b1e9408496706bea15f602ca4c626650ef87
Redyf <mateusalvespereira7@gmail.com>
2023-05-18 20:26:02 -0300

    New flake inputs + imports

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -24,6 +24,7 @@
   # Imports
   imports = [
     ./terminal
+    ../pkgs
   ];
 
   # Overlays
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,4 @@
-{ pkgs }:
+{ pkgs, ... }:
 with pkgs; {
-  sfmono-nf = callPackage ./sfmono-nf.nix { };
   xwaylandvideobridge = callPackage ./xwaylandvideobridge.nix { };
 }
