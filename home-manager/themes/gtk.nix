e73ce2b02a9a04f1787f4da7f70256024f40af50
Redyf <mateusalvespereira7@gmail.com>
2023-05-19 12:30:14 -0300

    gtk.nix module working

---
+++ home-manager/themes/gtk.nix
@@ -0,0 +1,21 @@
+{ config
+, pkgs
+, lib
+, ...
+}: {
+  gtk = {
+    enable = true;
+    cursorTheme = {
+      name = "macOS-BigSur";
+      package = pkgs.apple-cursor;
+    };
+    theme = {
+      name = "whitesur-gtk-theme";
+      package = pkgs.whitesur-gtk-theme;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-folders;
+    };
+  };
+}
