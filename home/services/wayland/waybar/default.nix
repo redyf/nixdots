68619a5253e208940fbf1c0fd4d697685f670898
Redyf <mateusalvespereira7@gmail.com>
2023-05-22 10:40:02 -0300

    Scripts for hyprland and waybar + working swww theme-changer

---
+++ home/services/wayland/waybar/default.nix
@@ -0,0 +1,41 @@
+{ pkgs
+, lib
+, config
+, osConfig
+, inputs
+, ...
+}:
+with lib; let
+  waybar_config = import ./config.nix { inherit osConfig config lib pkgs; };
+  waybar_style = import ./style.nix { inherit (config) colorscheme; };
+in
+{
+  home.packages = with pkgs; [
+    python39Packages.requests
+  ];
+  programs.waybar = {
+    enable = true;
+    systemd.enable = true;
+    # package = inputs.hyprland.packages.${pkgs.system}.waybar-hyprland;
+
+    /*
+      pkgs.waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ ["-Dexperimental=true"];
+      patchPhase = ''
+      substituteInPlace src/modules/wlr/workspace_manager.cpp --replace "zext_workspace_handle_v1_activate(workspace_handle_);" "const std::string command = \"${config.wayland.windowManager.hyprland.package}/bin/hyprctl dispatch workspace \" + name_; system(command.c_str());"
+      '';
+      });
+    */
+
+    settings = waybar_config;
+    style = waybar_style;
+  };
+
+  # nixpkgs.overlays = [
+  #   (self: super: {
+  #     waybar = super.waybar.overrideAttrs (oldAttrs: {
+  #       mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+  #     });
+  #   })
+  # ];
+}
---
+++ home/services/wayland/waybar/scripts/getnotification.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+GREEN='\e[1;32m'
+NC='\033[0m' # No Color
+GREY='\e[0;90m'
+
+# Get latest notification from dbus-monitor
+
+echo "timestamp: 1 
+icon: 
+appname: waybar
+summary: Hello!
+body: " >~/.config/waybar/store/lastnotif
+
+dbus-monitor "interface='org.freedesktop.Notifications'" | grep --line-buffered "member=Notify\|string" | while read line; do
+    printf "${GREY}debug: $line${NC}\n"
+    if [[ $line == *"member=Notify"* ]]; then
+        # Remove everything before the timestamp and after the . sign
+        timestamp=$(echo $line | sed 's/.*time=\([0-9]*\).*/\1/')
+        printf "\n${GREEN}timestamp:${NC} $timestamp\n"
+        sed -i "1s|.*|timestamp: $timestamp|" ~/.config/waybar/store/lastnotif
+        linenumber=0
+    else
+        if [[ $line == *"string"* ]]; then
+            linenumber=$((linenumber + 1))
+
+            if [ $linenumber -eq 1 ]; then
+                appname=$(echo "$line" | sed 's/string //g; s/\"//g')
+                printf "${GREEN}appname:${NC} $appname\n"
+                sed -i "2s|.*|appname: $appname|" ~/.config/waybar/store/lastnotif
+            fi
+
+            if [ $linenumber -eq 2 ]; then
+                icon=$(echo $line | sed 's/string //g; s/\"//g')
+                printf "${GREEN}icon:${NC} $icon\n"
+                sed -i "3s|.*|icon: $icon|" ~/.config/waybar/store/lastnotif
+            fi
+
+            if [ $linenumber -eq 3 ]; then
+                summary=$(echo $line | sed 's/string //g; s/\"//g')
+                printf "${GREEN}summary:${NC} $summary\n"
+                sed -i "4s|.*|summary: $summary|" ~/.config/waybar/store/lastnotif
+            fi
+
+            if [ $linenumber -eq 4 ]; then
+                body=$(echo $line | sed 's/string //g; s/\"//g')
+                printf "${GREEN}body:${NC} $body\n"
+                sed -i "5s|.*|body: $body|" ~/.config/waybar/store/lastnotif
+            fi
+        fi
+    fi
+done
