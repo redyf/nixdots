26397ba2aec8ef64a08b00c341cd57d1c615235e
Redyf <mateusalvespereira7@gmail.com>
2023-05-26 20:15:34 -0300

    First dev shell for python dev and new python packages

--- home/dev/python/python.nix
+++ home/dev/python/python.nix
@@ -1,17 +1,13 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    #Python/pip packages
     (python311.withPackages (ps:
       with ps; [
-        pip # Python package manager
         black # Python formatter
         tqdm
         pynvim
         flake8 # Linter for Python
         pygobject3 # Python bindings for Glib
-        gst-python # Pyton bindings for GStreamer
-        ascii-magic
-        gobject-introspection # A middleware layer between C libraries and language bindings
+        gst-python # Python bindings for GStreamer
       ]))
   ];
 }
