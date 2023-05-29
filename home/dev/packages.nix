45a448c2053e21d253f0261981f99fcd8d44a2dd
Redyf <mateusalvespereira7@gmail.com>
2023-05-29 15:05:39 -0300

    New colorpicker icon for waybar + new wallpapers and hyprpicker

--- home/dev/packages.nix
+++ home/dev/packages.nix
@@ -143,6 +143,7 @@
     slurp # Works with grim to screenshot on wayland
     ffmpeg_5 # A complete, cross-platform solution to record, convert and stream audio and video
     obs-studio # Livestreams
+    hyprpicker # Color picker for wayland
     wl-clipboard # Enables copy/paste on wayland
 
     # Gaming
