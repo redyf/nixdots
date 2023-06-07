d3bace1e936d57a548bdffd7f08b010fde8f54e4
Redyf <mateusalvespereira7@gmail.com>
2023-06-07 19:25:45 -0300

    Flake for laptop config

--- home/pkgs/default.nix
+++
@@ -1,7 +0,0 @@
-self: (
-  super: {
-    ly = super.callPackage ../pkgs/ly {};
-    sf-mono-liga-src = super.callPackage ./sf-mono-liga.nix {};
-    xwaylandvideobridge = super.callPackage ./xwaylandvideobridge.nix {};
-  }
-)
