cb6862ec84e65f408027b0093c3f0e73d03a1671
Redyf <mateusalvespereira7@gmail.com>
2023-06-15 21:22:21 -0300

    Improved startup time + fixed doas perms for git by installing git system-wide

--- home/desktop/dev/nix/nix.nix
+++ home/desktop/dev/nix/nix.nix
@@ -4,7 +4,6 @@
     alejandra # Nix formatter written in Rust
     statix # Lints and suggestions for the nix programming language
     nil # Nix LSP
-
     # Nix utils
     nix-init # Command line tool to generate Nix packages from URLs
     nix-prefetch-git # Script used to obtain source hashes for fetchgit
