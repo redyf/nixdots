f48db1992d7287527b2e16aa7c090e131aa52035
Redyf <mateusalvespereira7@gmail.com>
2023-06-17 01:21:22 -0300

    Improved nvim module + commented unused packages

--- home/desktop/system/fonts/default.nix
+++ home/desktop/system/fonts/default.nix
@@ -6,20 +6,13 @@
   ...
 }: {
   home.packages = with pkgs; [
-    # Fonts
-    go-font
     nerdfonts
     noto-fonts
-    dejavu_fonts
     font-awesome
     maple-mono-NF
-    liberation_ttf
-    powerline-fonts
-    cantarell-fonts
-    source-code-pro
     fira-code-symbols
     powerline-symbols
     material-design-icons
-    (nerdfonts.override {fonts = ["IBMPlexMono" "CascadiaCode" "FiraCode" "JetBrainsMono" "Iosevka"];})
+    (nerdfonts.override {fonts = ["FiraCode" "JetBrainsMono"];})
   ];
 }
