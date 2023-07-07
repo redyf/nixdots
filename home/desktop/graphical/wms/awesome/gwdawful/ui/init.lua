e1e4fb0c40756ff77e57d0e5ccee42f13329e2da
Redyf <mateusalvespereira7@gmail.com>
2023-07-07 10:42:53 -0300

    Catppuccin-Espresso colorscheme for foot term

--- home/desktop/graphical/wms/awesome/gwdawful/ui/init.lua
+++
@@ -1,9 +0,0 @@
------------------
--- ui settings --
------------------
-
-require('ui.notification')
-require('ui.menu')
-require('ui.bar')
-require('ui.titles')
-require('ui.dashboard')
--- home/desktop/terminal/foot/default.nix
+++ home/desktop/terminal/foot/default.nix
@@ -31,8 +31,29 @@ _: {
           beam-thickness = "1.5";
         };
         colors = {
+          # Catppuccin Espresso
+          # alpha = "1.0";
+          # foreground = "CDD6F4"; # Text
+          # background = "11111B"; # Base
+          # regular0 = "45475A"; # Surface 1
+          # regular1 = "F38BA8"; # red
+          # regular2 = "A6E3A1"; # green
+          # regular3 = "F9E2AF"; # yellow
+          # regular4 = "89B4FA"; # blue
+          # regular5 = "F5C2E7"; # pink
+          # regular6 = "94E2D5"; # teal
+          # regular7 = "BAC2DE"; # Subtext 1
+          # bright0 = "585B70"; # Surface 2
+          # bright1 = "F38BA8"; # red
+          # bright2 = "A6E3A1"; # green
+          # bright3 = "F9E2AF"; # yellow
+          # bright4 = "89B4FA"; # blue
+          # bright5 = "F5C2E7"; # pink
+          # bright6 = "94E2D5"; # teal
+          # bright7 = "A6ADC8"; # Subtext 0
+
           # Jabuti
-          alpha = "0.5";
+          alpha = "0.8";
           foreground = "d9e0ee";
           background = "292a37";
           ## Normal/regular colors (color palette 0-7)
