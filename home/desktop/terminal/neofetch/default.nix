adbdeab851d0d31c31dbb983c158dfc1c399a521
Redyf <mateusalvespereira7@gmail.com>
2023-07-29 23:37:05 -0300

    New neofetch themes + packages

--- home/desktop/terminal/neofetch/default.nix
+++ home/desktop/terminal/neofetch/default.nix
@@ -8,4 +8,5 @@
   # home.file.".config/neofetch/config.conf".text = import ./config.nix;
   # home.file.".config/neofetch/config.conf".text = import ./minimal.nix;
   home.file.".config/neofetch/config.conf".text = import ./clean.nix;
+  # home.file.".config/neofetch/config.conf".text = import ./geometrical.nix;
 }
