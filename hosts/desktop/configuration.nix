21df414b2bfa0cbed3c4c53046b1a3ecf13552d3
Will Byrne <will.alex.byrne@gmail.com>
2023-08-09 22:50:11 +0100

    enable ssh

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -335,7 +335,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
