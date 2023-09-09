6ffaa301aa306958a272422b50048f42f43430ee
Redyf <mateusalvespereira7@gmail.com>
2023-09-08 23:18:31 -0300

    Added tofi instead of bemenu + removed unused bar

--- home/desktop/graphical/wms/hyprland/default.nix
+++ home/desktop/graphical/wms/hyprland/default.nix
@@ -224,7 +224,8 @@ in {
 
         "SUPER,RETURN,exec, foot"
         ",Print,exec, ~/.config/hypr/scripts/screenshot.sh"
-        "SUPER,space,exec, bemenu-run"
+        # "SUPER,space,exec, bemenu-run"
+        "SUPER,space,exec, tofi-drun --drun-launch=true"
         # SUPER,space,exec,wofi --show drun -I -s ~/.config/wofi/style.css DP-3
         # "SUPER SHIFT,V,exec,~/.config/eww/fool_moon/bar/scripts/widgets toggle-clip"
         # "SUPER SHIFT,C,exec,~/.config/hypr/scripts/wallpaper_picker"
