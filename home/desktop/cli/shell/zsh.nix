d68e10fefeb042ffc2c5e8fcce6eec269029a224
Redyf <mateusalvespereira7@gmail.com>
2023-10-31 23:00:43 -0300

    Changed font for foot terminal + fixed emacs module

--- home/desktop/cli/shell/zsh.nix
+++ home/desktop/cli/shell/zsh.nix
@@ -15,7 +15,6 @@
       export PATH=$PATH:~/.local/bin/
       export PATH=/tmp/lazy-lvim/bin:$PATH
       export PATH="$HOME/.emacs.d/bin:$PATH"
-      export PATH="$HOME/doom/bin:$PATH"
       export PATH="$PATH:/home/redyf/.nix-profile/bin/elixir"
       export PATH="$PATH:/home/redyf/.nix-profile/bin/swww"
       export PATH="$PATH:/home/redyf/.nix-profile/bin/swww-daemon"
