24bbb33eb629456f1d0a5fa87cbacfd6d7e246e5
Redyf <mateusalvespereira7@gmail.com>
2023-11-11 22:42:58 -0300

    Updated config

--- home/desktop/system/fonts/default.nix
+++ home/desktop/system/fonts/default.nix
@@ -1,24 +1,14 @@
-{
-  inputs,
-  config,
-  pkgs,
-  lib,
-  ...
-}: {
+{pkgs, ...}: {
   home.packages = with pkgs; [
-    # Fonts
-    # go-font
     # apl386
     noto-fonts
     dejavu_fonts
     font-awesome
-    # courier-prime
     # powerline-fonts
     # cantarell-fonts
     fira-code-symbols
     powerline-symbols
     material-design-icons
-    # helvetica-neue-lt-std
     (nerdfonts.override {fonts = ["IBMPlexMono" "CascadiaCode" "FiraCode" "JetBrainsMono" "SourceCodePro"];})
   ];
 }
