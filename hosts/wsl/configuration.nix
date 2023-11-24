83752eb862380ea6fa62b8892bdf28786f78f1b2
Mateus Alves <98139059+redyf@users.noreply.github.com>
2023-11-23 21:57:50 -0300

    Update configuration.nix
    Removed extraLocaleSettings

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -79,10 +79,8 @@
     LC_MEASUREMENT = "pt_BR.UTF-8";
     LC_MONETARY = "pt_BR.UTF-8";
     LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
     LC_PAPER = "pt_BR.UTF-8";
     LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
   };
 
   # Enable programs
