d73b4021563159d0c3a0c979cb1214fac4f38a8b
Redyf <mateusalvespereira7@gmail.com>
2023-12-05 21:15:15 -0300

    New env variables for hyprland + commented env variables from configuration.nix

--- home/desktop/graphical/wms/hyprland/default.nix
+++ home/desktop/graphical/wms/hyprland/default.nix
@@ -291,7 +291,11 @@ in {
     extraConfig = ''
            # source = ~/.config/hypr/themes/catppuccin-macchiato.conf
            # source = ~/.config/hypr/themes/oxocarbon.conf
-
+           env = LIBVA_DRIVER_NAME,nvidia
+           env = XDG_SESSION_TYPE,wayland
+           env = GBM_BACKEND,nvidia-drm
+           env = __GLX_VENDOR_LIBRARY_NAME,nvidia
+           env = WLR_NO_HARDWARE_CURSORS,1
       #     # will switch to a submap called resize
       #     bind=$mainMod,R,submap,resize
       #
