2211592e0c80212612cb5fb558cdcbcf0f0fdd58
Redyf <mateusalvespereira7@gmail.com>
2023-12-21 13:49:10 -0300

    New tokyonight theme for st

--- home/desktop/graphical/wms/hyprland/default.nix
+++ home/desktop/graphical/wms/hyprland/default.nix
@@ -211,14 +211,14 @@ in {
         # "SUPER $mainMod SHIFT, 9, movetoworkspacesilent, 9"
         # "SUPER $mainMod SHIFT, 0, movetoworkspacesilent, 10"
 
-        "SUPER,RETURN,exec,wezterm"
+        "SUPER,RETURN,exec,foot"
+        "ALT,RETURN,exec,kitty"
         "SUPER,n,exec,neovide"
         "SUPER,e,exec,emacsclient -c -a 'emacs'"
         "SUPER,o,exec,anytype --use-gl=desktop"
         ",Print,exec,~/.config/hypr/scripts/screenshot.sh"
         "SUPER,space,exec,bemenu-run"
         # "SUPER,z,exec,waybar"
-        # "SUPER,n,exec,~/.local/bin/lvimn"
         # "SUPER,space,exec, tofi-drun --drun-launch=true"
         # SUPER,space,exec,wofi --show drun -I -s ~/.config/wofi/style.css DP-3
         # "SUPER SHIFT,V,exec,~/.config/eww/fool_moon/bar/scripts/widgets toggle-clip"
