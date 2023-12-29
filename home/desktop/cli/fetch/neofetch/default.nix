019cb55742674e14cfc063b835c38a45aa028d39
Redyf <mateusalvespereira7@gmail.com>
2023-12-29 00:43:12 -0300

    flake update

--- home/desktop/cli/fetch/neofetch/default.nix
+++ home/desktop/cli/fetch/neofetch/default.nix
@@ -1,5 +1,10 @@
 {pkgs, ...}: {
-  home.packages = [pkgs.neofetch];
+  home.packages = with pkgs; [
+    neofetch
+    imagemagick # Dependency for neofetch so it displays images
+    libsixel # Dependency for neofetch so it displays images
+    w3m
+  ];
   # home.file.".config/neofetch/config.conf".text = import ./config.nix;
   # home.file.".config/neofetch/config.conf".text = import ./minimal.nix;
   home.file.".config/neofetch/config.conf".text = import ./clean.nix;
--- home/desktop/dev/misc.nix
+++ home/desktop/dev/misc.nix
@@ -2,34 +2,14 @@
   home.packages = with pkgs; [
     # TODO: Convert all packages to nix modules
 
-    # Design
-    # figma-linux
-
-    # Modeling
-    # dia
-
     # System Utils
     mpd
     mpv
-    w3m
-    libsixel # Dependency for neofetch so it displays images
-    imagemagick # Dependency for neofetch so it displays images
-    file
-    fuse
     tree
-    peek
-    timer # CLI sleep progress app
-    light # GNU/Linux application to control backlights
+    peek # Animated GIF screen recorder
     xclip
-    chafa # Transform images into colorful ascii (works with gifs too)
     unzip # Unzip files using the terminal
-    lolcat
-    speechd
-    libnotify
-    # img2pdf # Convert images to pdf
-    findutils
-    # flameshot
-    traceroute
+    traceroute # Tool to access the X clipboard from a console application
     tree-sitter # A parser generator tool and an incremental parsing library
     appimage-run # Run appimage files in the terminal
     polkit_gnome
@@ -40,31 +20,20 @@
     zathura # PDF viewer
 
     # Browsers
-    brave
     firefox
-
-    # Notes
-    # anytype
-    # obsidian
 
     # Anime/Manga
     ani-cli # A cli tool to browse and play anime
-    mangal # A fancy CLI app written in Go which scrapes, downloads and packs manga into different formats
+    # mangal # A fancy CLI app written in Go which scrapes, downloads and packs manga into different formats
 
     # Rice
     swww # Cool wallpapers/gifs transitions
     dunst # Notifications for your system
-    # sxhkd # BSPWM keybindings
     bemenu # Dmenu for wayland
     pcmanfm # Best GUI file manager
     cmatrix
-    # polybar # Bar i use on X11
     onefetch
     nitrogen # Wallpaper utility for X11
-    # picom-next # Compositor for X11
-    # eww-wayland # Cool widgets for wayland
-    # lxappearance # Gui software to change icons/themes
-    # brightnessctl
 
     # Screenshot + extra utils
     grim # Screenshot tool for hyprland
@@ -79,7 +48,6 @@
     # libpulseaudio
 
     # Audio/Music
-    # spotify
     easyeffects
     pavucontrol
   ];
