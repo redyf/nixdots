5c530d17cf92a2121b9a7d595acf4319cde68975
Redyf <mateusalvespereira7@gmail.com>
2024-01-08 17:45:13 -0300

    feat: Add rosé pine colorscheme for terminals

--- home/desktop/cli/terminal/kitty/default.nix
+++ home/desktop/cli/terminal/kitty/default.nix
@@ -1,11 +1,16 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [kitty];
-  xdg.configFile."kitty/kitty.conf".text = import ./kitty.nix;
-  xdg.configFile."kitty/themes/challenger_deep.conf".text = import ./challenger-deep.nix;
-  xdg.configFile."kitty/themes/dark-decay.conf".text = import ./dark-decay.nix;
-  xdg.configFile."kitty/themes/decayce.conf".text = import ./decayce.nix;
-  xdg.configFile."kitty/themes/lunar.conf".text = import ./lunar.nix;
-  xdg.configFile."kitty/themes/mocha.conf".text = import ./mocha.nix;
-  xdg.configFile."kitty/themes/macchiato.conf".text = import ./macchiato.nix;
-  xdg.configFile."kitty/themes/tokyonight_night.conf".text = import ./tokyonight_night.nix;
+  xdg = {
+    configFile = {
+      "kitty/kitty.conf".text = import ./kitty.nix;
+      "kitty/themes/challenger_deep.conf".text = import ./challenger-deep.nix;
+      "kitty/themes/dark-decay.conf".text = import ./dark-decay.nix;
+      "kitty/themes/decayce.conf".text = import ./decayce.nix;
+      "kitty/themes/lunar.conf".text = import ./lunar.nix;
+      "kitty/themes/mocha.conf".text = import ./mocha.nix;
+      "kitty/themes/macchiato.conf".text = import ./macchiato.nix;
+      "kitty/themes/tokyonight_night.conf".text = import ./tokyonight_night.nix;
+      "kitty/themes/rose-pine.conf".text = import ./rose-pine.nix;
+    };
+  };
 }
