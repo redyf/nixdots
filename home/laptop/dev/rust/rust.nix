34f42dafe6ce17170d8f2024f34efe8a1bd4283f
Redyf <mateusalvespereira7@gmail.com>
2024-01-12 16:17:50 -0300

    Delete laptop configuration as I won't use it

--- home/laptop/dev/rust/rust.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    rustc
-    cargo
-  ];
-}
--- home/laptop/terminal/pfetch/default.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, ... }: {
-  home = {
-    packages = with pkgs; [ pfetch ];
-    sessionVariables.PF_INFO = "ascii title os kernel uptime shell term desktop scheme palette";
-  };
-}
