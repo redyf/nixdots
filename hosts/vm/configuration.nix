bfe21027d026ba116a85396a20b4e716a55d49ac
Redyf <mateusalvespereira7@gmail.com>
2024-01-17 18:37:18 -0300

    feat: Update README

--- hosts/vm/configuration.nix
+++ hosts/vm/configuration.nix
@@ -3,7 +3,7 @@
 # To be able to connect with ssh enable port forwarding with:
 # QEMU_NET_OPTS="hostfwd=tcp::2222-:22" ./result/bin/run-nixos-vm
 # Then connect with ssh -p 2222 guest@localhost
-{ nixpkgs, lib, config, pkgs, ... }:
+{ lib, config, pkgs, ... }:
 {
   # Internationalisation options
   i18n.defaultLocale = "pt_BR.UTF-8";
