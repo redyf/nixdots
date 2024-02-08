f4d053b5da603dbfea88160efa438a4ce216b95f
Redyf <mateusalvespereira7@gmail.com>
2024-02-08 12:27:08 -0300

    feat: Improve copy mode for tmux
    The default keybinds for copy mode with the plugin yanky aren't that good so added new ones instead (Thanks Dreams of Code!)

--- modules/nixos/cli-apps/tmux/default.nix
+++ modules/nixos/cli-apps/tmux/default.nix
@@ -32,6 +32,9 @@ in
       ];
       extraConfig =
         ''
+          #--------------------------------------------------------------------------
+          # Keybinds
+          #--------------------------------------------------------------------------
                     # Shift Alt vim keys to switch windows
                     bind -n M-H previous-window
                     bind -n M-L next-window
@@ -43,6 +46,11 @@ in
                     # Open panes in current directory
                     bind '"' split-window -v -c "#{pane_current_path}"
                     bind % split-window -h -c "#{pane_current_path}"
+
+                    # Copy mode
+                    bind-key -T copy-mode-vi v send-keys -X begin-selection
+                    bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+                    bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
 
           #--------------------------------------------------------------------------
           # Status line
