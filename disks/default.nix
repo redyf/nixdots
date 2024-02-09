7073da4ab806af1161ba94a56749067f8085720c
Redyf <mateusalvespereira7@gmail.com>
2024-02-08 22:25:31 -0300

    feat: Add new option for disko module

--- disks/default.nix
+++ disks/default.nix
@@ -1,43 +1,49 @@
-# If you'd saved this configuration in /tmp/disko-config.nix, and wanted to create a disk named /dev/nvme0n1, you would run the following command to partition, format and mount the disk.
+# If you'd saved this configuration in ./disks/default.nix, and wanted to create a disk named /dev/nvme0n1, you would run the following command to partition, format and mount the disk.
 
-# sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko /tmp/disko-config.nix
-
-# sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disks/default.nix --arg device '/dev/vda'
+# sudo nix --experimental-features "nix-command flakes" run github:nix-community/disko -- --mode disko ./disks/default.nix --arg device '"/dev/nvme0n1"'
 
 
 { lib
 , device ? throw "Set this to your disk device, e.g. /dev/sda"
 , ...
 }: {
-  disko.devices = {
-    disk.main = {
-      inherit device;
-      type = "disk";
-      content = {
-        type = "gpt";
-        partitions = {
-          boot = {
-            name = "boot";
-            size = "1M";
-            type = "EF02";
-          };
-          esp = {
-            name = "ESP";
-            size = "500M";
-            type = "EF00";
-            content = {
-              type = "filesystem";
-              format = "vfat";
-              mountpoint = "/boot";
+  disko = {
+    # Do not let Disko manage fileSystems.* config for NixOS.
+    # Reason is that Disko mounts partitions by GPT partition names, which are
+    # easily overwritten with tools like fdisk. When you fail to deploy a new
+    # config in this case, the old config that comes with the disk image will
+    # not boot either.
+    enableConfig = false;
+    devices = {
+      disk.main = {
+        inherit device;
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            boot = {
+              name = "boot";
+              size = "1M";
+              type = "EF02";
             };
-          };
-          root = {
-            name = "root";
-            size = "100%";
-            content = {
-              type = "filesystem";
-              format = "ext4";
-              mountpoint = "/";
+            esp = {
+              name = "ESP";
+              size = "500M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              name = "root";
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
             };
           };
         };
