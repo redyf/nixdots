ca54b91729897cb61b1e31d67a28a8e2c4b0b78a
Redyf <mateusalvespereira7@gmail.com>
2024-02-12 22:00:46 -0300

    refactor: Moving cli-apps to their respective modules
    I decided to move them all to their specific directories to make things easier to manage, they were in a single file with 501 lines.

---
+++ modules/nixos/cli-apps/zoxide/default.nix
@@ -0,0 +1,25 @@
+{
+  inputs,
+  options,
+  config,
+  pkgs,
+  lib,
+  system,
+  ...
+}:
+with lib;
+with lib.custom; let
+  cfg = config.cli-apps.zoxide;
+in {
+  options.cli-apps.zoxide = with types; {
+    enable = mkBoolOpt false "Enable or disable zoxide";
+  };
+
+  config = mkIf cfg.enable {
+    home.extraOptions.programs.zoxide = {
+      enable = true;
+      enableZshIntegration = true;
+      options = [];
+    };
+  };
+}
