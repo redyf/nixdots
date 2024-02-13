86a07f5e2dc599fe7429449b0234f8e2157f4a58
redyf <redyf@users.noreply.github.com>
2024-02-13 01:02:49 +0000

    Auto lint/format

--- modules/nixos/cli-apps/bat/default.nix
+++ modules/nixos/cli-apps/bat/default.nix
@@ -1,16 +1,16 @@
-{
-  inputs,
-  options,
-  config,
-  pkgs,
-  lib,
-  system,
-  ...
+{ inputs
+, options
+, config
+, pkgs
+, lib
+, system
+, ...
 }:
 with lib;
 with lib.custom; let
   cfg = config.cli-apps.bat;
-in {
+in
+{
   options.cli-apps.bat = with types; {
     enable = mkBoolOpt false "Enable or disable bat";
   };
@@ -22,23 +22,25 @@ in {
         pager = "less -FR";
         theme = "Catppuccin-mocha";
       };
-      themes = let
-        src = pkgs.fetchFromGitHub {
-          owner = "catppuccin";
-          repo = "bat";
-          rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
-          hash = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+      themes =
+        let
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "ba4d16880d63e656acced2b7d4e034e4a93f74b1";
+            hash = "sha256-6WVKQErGdaqb++oaXnY3i6/GuH2FhTgK0v4TN4Y0Wbw=";
+          };
+        in
+        {
+          Catppuccin-mocha = {
+            inherit src;
+            file = "Catppuccin-mocha.tmTheme";
+          };
+          Catppuccin-latte = {
+            inherit src;
+            file = "Catppuccin-latte.tmTheme";
+          };
         };
-      in {
-        Catppuccin-mocha = {
-          inherit src;
-          file = "Catppuccin-mocha.tmTheme";
-        };
-        Catppuccin-latte = {
-          inherit src;
-          file = "Catppuccin-latte.tmTheme";
-        };
-      };
     };
   };
 }
