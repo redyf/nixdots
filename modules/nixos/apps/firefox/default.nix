e33065fade9b7e72aab7612ca9ef320d8483e043
redyf <redyf@users.noreply.github.com>
2024-02-15 00:29:08 +0000

    Auto lint/format

--- modules/nixos/apps/firefox/default.nix
+++ modules/nixos/apps/firefox/default.nix
@@ -1,14 +1,14 @@
-{
-  options,
-  config,
-  pkgs,
-  lib,
-  ...
+{ options
+, config
+, pkgs
+, lib
+, ...
 }:
 with lib;
 with lib.custom; let
   cfg = config.apps.firefox;
-in {
+in
+{
   options.apps.firefox = with types; {
     enable = mkBoolOpt false "Enable or disable firefox browser";
   };
