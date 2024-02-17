f4cb9aadaebaf773ce27a33b344da2ea37f6a779
Redyf <mateusalvespereira7@gmail.com>
2024-02-17 12:50:56 -0300

    feat: Disable intellij for java module

--- modules/nixos/tools/languages/java/default.nix
+++ modules/nixos/tools/languages/java/default.nix
@@ -1,32 +1,31 @@
-{ options
-, config
-, lib
-, pkgs
-, ...
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
 }:
 with lib;
 with lib.custom; let
   cfg = config.tools.languages.java;
-in
-{
+in {
   options.tools.languages.java = with types; {
     enable = mkBoolOpt false "Enable java";
   };
 
-  config =
-    mkIf cfg.enable {
-      environment.systemPackages = with pkgs; [
-        jdk # Java dev kit
-        jdt-language-server # Jdtls integration
-        checkstyle # Linter
-        google-java-format # Formatter
-        vscode-extensions.vscjava.vscode-java-debug
-        vscode-extensions.vscjava.vscode-java-test
-        maven # Build automation tool for java
-        # spring-boot-cli
-      ];
-      tools = {
-        intellij = enabled;
-      };
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      jdk # Java dev kit
+      jdt-language-server # Jdtls integration
+      checkstyle # Linter
+      google-java-format # Formatter
+      vscode-extensions.vscjava.vscode-java-debug
+      vscode-extensions.vscjava.vscode-java-test
+      maven # Build automation tool for java
+      # spring-boot-cli
+    ];
+    tools = {
+      intellij = disabled;
     };
+  };
 }
