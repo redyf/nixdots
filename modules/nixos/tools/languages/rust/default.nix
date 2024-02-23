a9c42e2c152ca9bf7f3dd98bba219dc9dd258566
Redyf <mateusalvespereira7@gmail.com>
2024-02-22 21:16:01 -0300

    refactor: comment neovim-nightly

--- modules/nixos/tools/languages/rust/default.nix
+++ modules/nixos/tools/languages/rust/default.nix
@@ -1,14 +1,14 @@
-{ options
-, config
-, lib
-, pkgs
-, ...
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
 }:
 with lib;
 with lib.custom; let
   cfg = config.tools.languages.rust;
-in
-{
+in {
   options.tools.languages.rust = with types; {
     enable = mkBoolOpt false "Enable rust";
   };
@@ -18,7 +18,7 @@ in
       cargo # Downloads your Rust project's dependencies and builds your project
       clippy # A bunch of lints to catch common mistakes and improve your Rust code
       lldb # A next-generation high-performance debugger
-      # rust-analyzer # Lsp for rust
+      rust-analyzer # Lsp for rust
       rustc # Compiler for rust
       rustfmt # Formatter for rust language
       rustup # The Rust toolchain installer
