ffd59d9ab1205ff01ae08fa8974d321e4eecbe47
Redyf <mateusalvespereira7@gmail.com>
2024-02-22 21:17:15 -0300

    style: Formatting with alejandra

--- modules/nixos/tools/languages/lua/default.nix
+++ modules/nixos/tools/languages/lua/default.nix
@@ -1,28 +1,25 @@
-{ options
-, config
-, lib
-, pkgs
-, ...
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
 }:
 with lib;
 with lib.custom; let
   cfg = config.tools.languages.lua;
-in
-{
+in {
   options.tools.languages.lua = with types; {
     enable = mkBoolOpt false "Enable lua";
   };
 
-  config =
-    mkIf cfg.enable {
-      environment.systemPackages = with pkgs; [
-        # Lua packages
-        lua # Lua programming language
-        stylua # Lua formatter
-        selene # Lua linter written in rust
-        # luaformatter # Lua formatter (currently using this one)
-        lua-language-server # Lua LSP
-        luajitPackages.luarocks-nix # Package manager for Lua on Nix      
-      ];
-    };
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [
+      lua # Lua programming language
+      stylua # Lua formatter
+      selene # Lua linter written in rust
+      lua-language-server # Lua LSP
+      luajitPackages.luarocks-nix # Package manager for Lua on Nix
+    ];
+  };
 }
