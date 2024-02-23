2bf5531ea596c09f87f64ca7e1edbf1f04ca2aa7
Redyf <mateusalvespereira7@gmail.com>
2024-02-22 21:16:01 -0300

    refactor: comment neovim-nightly

--- modules/nixos/tools/languages/java/default.nix
+++ modules/nixos/tools/languages/java/default.nix
@@ -15,17 +15,18 @@ in {
 
   config = mkIf cfg.enable {
     environment.systemPackages = with pkgs; [
+      # TODO: Disabled some of them to install with mason
       jdk # Java dev kit
-      jdt-language-server # Jdtls integration
+      # jdt-language-server # Jdtls integration
       checkstyle # Linter
       google-java-format # Formatter
-      vscode-extensions.vscjava.vscode-java-debug
-      vscode-extensions.vscjava.vscode-java-test
+      # vscode-extensions.vscjava.vscode-java-debug
+      # vscode-extensions.vscjava.vscode-java-test
       maven # Build automation tool for java
       # spring-boot-cli
     ];
     tools = {
-      intellij = disabled;
+      intellij = enabled;
     };
   };
 }
