2fed6a323c30cbccf1060f4ed3fbcfb9103114aa
Redyf <mateusalvespereira7@gmail.com>
2024-02-22 21:17:15 -0300

    style: Formatting with alejandra

--- modules/nixos/desktop/addons/wezterm/default.nix
+++ modules/nixos/desktop/addons/wezterm/default.nix
@@ -1,16 +1,16 @@
-{ options
-, config
-, pkgs
-, lib
-, inputs
-, ...
+{
+  options,
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
 }:
 with lib;
 with lib.custom; let
   cfg = config.desktop.addons.wezterm;
   inherit (inputs.nix-colors.colorschemes.${builtins.toString config.desktop.colorscheme}) colors;
-in
-{
+in {
   options.desktop.addons.wezterm = with types; {
     enable = mkBoolOpt false "Enable or disable the wezterm terminal.";
   };
