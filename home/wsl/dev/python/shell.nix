4217dd25ff97e844883d4f8d83e78591ec76327b
Redyf <mateusalvespereira7@gmail.com>
2024-02-24 14:55:06 -0300

    feat: Go back to default flake structure
    Snowfall lib by Jake Hamilton is really good, however after trying it for a month I realized it wasn't for me.

---
+++ home/wsl/dev/python/shell.nix
@@ -0,0 +1,11 @@
+{ pkgs ? import <nixpkgs> { } }:
+let
+  my-python-packages = ps:
+    with ps; [
+      pip
+      pypresence
+      xlib
+    ];
+  my-python = pkgs.python3.withPackages my-python-packages;
+in
+my-python.env
