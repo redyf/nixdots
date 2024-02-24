da5fe111d58588998c5de7189119b825385b48c4
Redyf <mateusalvespereira7@gmail.com>
2024-02-24 15:32:04 -0300

    feat: Rename packages to pkgs

--- home/desktop/apps/insomnia/default.nix
+++ home/desktop/apps/insomnia/default.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    (pkgs.callPackage ../../../../packages/insomnia.nix {
+    (pkgs.callPackage ../../../../pkgs/insomnia.nix {
       src = insomnia; # kernel source is passed as a `specialArgs` and injected into this module.
     })
   ];
