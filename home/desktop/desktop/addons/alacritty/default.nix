d964b807e6511c0103ae654fb2fb2329dc0773f4
Redyf <mateusalvespereira7@gmail.com>
2024-03-05 23:02:44 -0300

    refactor: Break colorschemes into separate files

--- home/desktop/desktop/addons/alacritty/default.nix
+++ home/desktop/desktop/addons/alacritty/default.nix
@@ -1,17 +1,22 @@
-_: {
+_: let
+  catppuccin = import ./catppuccin.nix;
+  rose-pine = import ./rose-pine.nix;
+  tokyonight = import ./tokyonight.nix;
+in {
   programs.alacritty = {
     enable = true;
     settings = {
+      colors = catppuccin.colors;
       cursor = {
         style = {
           blinking = "Never";
         };
       };
       font = {
-        size = 15;
+        size = 13;
         normal = {
           family = "MonoLisa";
-          style = "Medium";
+          style = "Medium Italic";
         };
         bold = {
           family = "MonoLisa";
@@ -35,96 +40,12 @@ _: {
         };
       };
       window = {
-        opacity = 0.98;
+        opacity = 1;
         decorations = "none";
         padding = {
           x = 10;
           y = 10;
         };
-      };
-      colors = {
-        primary = {
-          background = "#1E1E2E";
-          foreground = "#CDD6F4";
-          dim_foreground = "#CDD6F4";
-          bright_foreground = "#CDD6F4";
-        };
-        cursor = {
-          text = "#1E1E2E";
-          cursor = "#F5E0DC";
-        };
-        vi_mode_cursor = {
-          text = "#1E1E2E";
-          cursor = "#B4BEFE";
-        };
-        search = {
-          matches = {
-            foreground = "#1E1E2E";
-            background = "#A6ADC8";
-          };
-          focused_match = {
-            foreground = "#1E1E2E";
-            background = "#A6E3A1";
-          };
-        };
-        footer_bar = {
-          foreground = "#1E1E2E";
-          background = "#A6ADC8";
-        };
-        hints = {
-          start = {
-            foreground = "#1E1E2E";
-            background = "#F9E2AF";
-          };
-          end = {
-            foreground = "#1E1E2E";
-            background = "#A6ADC8";
-          };
-        };
-        selection = {
-          text = "#1E1E2E";
-          background = "#F5E0DC";
-        };
-        normal = {
-          black = "#45475A";
-          red = "#F38BA8";
-          green = "#A6E3A1";
-          yellow = "#F9E2AF";
-          blue = "#89B4FA";
-          magenta = "#F5C2E7";
-          cyan = "#94E2D5";
-          white = "#BAC2DE";
-        };
-        bright = {
-          black = "#585B70";
-          red = "#F38BA8";
-          green = "#A6E3A1";
-          yellow = "#F9E2AF";
-          blue = "#89B4FA";
-          magenta = "#F5C2E7";
-          cyan = "#94E2D5";
-          white = "#A6ADC8";
-        };
-        dim = {
-          black = "#45475A";
-          red = "#F38BA8";
-          green = "#A6E3A1";
-          yellow = "#F9E2AF";
-          blue = "#89B4FA";
-          magenta = "#F5C2E7";
-          cyan = "#94E2D5";
-          white = "#BAC2DE";
-        };
-        indexed_colors = [
-          {
-            index = 17;
-            color = "#F5E0DC";
-          }
-          {
-            index = 16;
-            color = "#FAB387";
-          }
-        ];
       };
       keyboard.bindings = [
         {
@@ -193,109 +114,5 @@ _: {
         }
       ];
     };
-    # colors = {
-    #   primary = {
-    #     # mountain
-    #     # foreground = "#f0f0f0";
-    #     # background = "#0f0f0f";
-    #     # rose-pine
-    #     # foreground = "#e0def4";
-    #     # background = "#191724";
-    #     # dim_foreground = "#908caa";
-    #     # bright_foreground = "#e0def4";
-    #   };
-    #   # rose-pine
-    #   cursor = {
-    #     text = "#e0def4";
-    #     cursor = "#524f67";
-    #   };
-    #   vi_mode_cursor = {
-    #     text = "#e0def4";
-    #     cursor = "#524f67";
-    #   };
-    #   search = {
-    #     matches = {
-    #       foreground = "#908caa";
-    #       background = "#26233a";
-    #     };
-    #     focused_match = {
-    #       foreground = "#191724";
-    #       background = "#ebbcba";
-    #     };
-    #   };
-    #   hints = {
-    #     start = {
-    #       foreground = "#908caa";
-    #       background = "#1f1d2e";
-    #     };
-    #     end = {
-    #       foreground = "#6e6a86";
-    #       background = "#1f1d2e";
-    #     };
-    #   };
-    #   line_indicator = {
-    #     foreground = "None";
-    #     background = "None";
-    #   };
-    #   footer_bar = {
-    #     foreground = "#e0def4";
-    #     background = "#1f1d2e";
-    #   };
-    #   selection = {
-    #     text = "#e0def4";
-    #     background = "#403d52";
-    #   };
-    #   normal = {
-    #     # mountain
-    #     # black = "#262626";
-    #     # red = "#ac8a8c";
-    #     # green = "#8aac8b";
-    #     # yellow = "#aca98a";
-    #     # blue = "#8f8aac";
-    #     # magenta = "#ac8aac";
-    #     # cyan = "#8aacac";
-    #     # white = "#e7e7e7";
-    #     # rose-pine
-    #     # black = "#26233a";
-    #     # red = "#eb6f92";
-    #     # green = "#31748f";
-    #     # yellow = "#f6c177";
-    #     # blue = "#9ccfd8";
-    #     # magenta = "#c4a7e7";
-    #     # cyan = "#ebbcba";
-    #     # white = "#e0def4";
-    #   };
-    #   bright = {
-    #     # mountain
-    #     # black = "#4c4c4c";
-    #     # red = "#c49ea0";
-    #     # green = "#9ec49f";
-    #     # yellow = "#c4c19e";
-    #     # blue = "#a39ec4";
-    #     # magenta = "#c49ec4";
-    #     # cyan = "#9ec3c4";
-    #     # white = "#f5f5f5";
-    #     # rose-pine
-    #     # black = "#6e6a86";
-    #     # red = "#eb6f92";
-    #     # green = "#31748f";
-    #     # yellow = "#f6c177";
-    #     # blue = "#9ccfd8";
-    #     # magenta = "#c4a7e7";
-    #     # cyan = "#ebbcba";
-    #     # white = "#e0def4";
-    #   };
-    #   dim = {
-    #     # rose-pine
-    #     black = "#6e6a86";
-    #     red = "#eb6f92";
-    #     green = "#31748f";
-    #     yellow = "#f6c177";
-    #     blue = "#9ccfd8";
-    #     magenta = "#c4a7e7";
-    #     cyan = "#ebbcba";
-    #     white = "#e0def4";
-    #   };
-    # };
   };
 }
---
+++ home/desktop/desktop/addons/alacritty/rose-pine.nix
@@ -0,0 +1,80 @@
+{
+  colors = {
+    primary = {
+      foreground = "#e0def4";
+      background = "#191724";
+      dim_foreground = "#908caa";
+      bright_foreground = "#e0def4";
+    };
+    cursor = {
+      text = "#e0def4";
+      cursor = "#524f67";
+    };
+    vi_mode_cursor = {
+      text = "#e0def4";
+      cursor = "#524f67";
+    };
+    search = {
+      matches = {
+        foreground = "#908caa";
+        background = "#26233a";
+      };
+      focused_match = {
+        foreground = "#191724";
+        background = "#ebbcba";
+      };
+    };
+    hints = {
+      start = {
+        foreground = "#908caa";
+        background = "#1f1d2e";
+      };
+      end = {
+        foreground = "#6e6a86";
+        background = "#1f1d2e";
+      };
+    };
+    line_indicator = {
+      foreground = "None";
+      background = "None";
+    };
+    footer_bar = {
+      foreground = "#e0def4";
+      background = "#1f1d2e";
+    };
+    selection = {
+      text = "#e0def4";
+      background = "#403d52";
+    };
+    normal = {
+      black = "#26233a";
+      red = "#eb6f92";
+      green = "#31748f";
+      yellow = "#f6c177";
+      blue = "#9ccfd8";
+      magenta = "#c4a7e7";
+      cyan = "#ebbcba";
+      white = "#e0def4";
+    };
+    bright = {
+      black = "#6e6a86";
+      red = "#eb6f92";
+      green = "#31748f";
+      yellow = "#f6c177";
+      blue = "#9ccfd8";
+      magenta = "#c4a7e7";
+      cyan = "#ebbcba";
+      white = "#e0def4";
+    };
+    dim = {
+      black = "#6e6a86";
+      red = "#eb6f92";
+      green = "#31748f";
+      yellow = "#f6c177";
+      blue = "#9ccfd8";
+      magenta = "#c4a7e7";
+      cyan = "#ebbcba";
+      white = "#e0def4";
+    };
+  };
+}
