3e44e6bc4968314ea0bffe9eaa337be9ce643e39
Redyf <mateusalvespereira7@gmail.com>
2024-03-05 23:02:07 -0300

    feat: Add catppuccin + tokyonight colorscheme

--- home/desktop/cli-apps/tmux/default.nix
+++ home/desktop/cli-apps/tmux/default.nix
@@ -1,42 +1,15 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
-  home.packages = with pkgs; [
-    # https://github.com/edr3x/tmux-sessionizer?tab=readme-ov-file#tmux-sessionizer
-    tmux-sessionizer
-    # Script to find files with tmux in vim
-    (writeShellScriptBin "tmux-sessionizer-script" ''
-          if [[ $# -eq 1 ]]; then
-          selected=$1
-      else
-          selected=$(find ~/projects ~/tests -mindepth 1 -maxdepth 1 -type d | fzf)
-      fi
-
-      if [[ -z $selected ]]; then
-          exit 0
-      fi
-
-      selected_name=$(basename "$selected" | tr . _)
-      tmux_running=$(pgrep tmux)
-
-      if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-          tmux new-session -s $selected_name -c $selected
-          exit 0
-      fi
-
-      if ! tmux has-session -t=$selected_name 2> /dev/null; then
-          tmux new-session -ds $selected_name -c $selected
-      fi
-
-      if [[ -z $TMUX ]]; then
-          tmux attach -t $selected_name
-      else
-          tmux switch-client -t $selected_name
-      fi
-    '')
-  ];
+{pkgs, ...}: let
+  catppuccin-tmux = pkgs.tmuxPlugins.mkTmuxPlugin {
+    pluginName = "catppuccin";
+    version = "0.0.1.dmmulroy-fork";
+    src = pkgs.fetchFromGitHub {
+      owner = "dmmulroy";
+      repo = "catppuccin-tmux";
+      rev = "ec8df6268a6e5271c693ea34fc545000cec1fced";
+      sha256 = "sha256-I8rAg3ecLxW4emtY1VpKpUMIaXfSks416LLkHtLZSsI=";
+    };
+  };
+in {
   programs.tmux = {
     enable = true;
     clock24 = true;
@@ -47,12 +20,14 @@
     prefix = "C-Space";
     mouse = true;
     plugins = with pkgs.tmuxPlugins; [
-      # catppuccin
+      # {
+      #   plugin = catppuccin-tmux;
+      #   extraConfig = builtins.readFile ./catppuccin.conf;
+      # }
       yank
       sensible
       tmux-fzf
       vim-tmux-navigator
-      inputs.tmux-sessionx.packages.${pkgs.system}.default
     ];
     extraConfig = ''
       #--------------------------------------------------------------------------
@@ -80,40 +55,69 @@
       # Place status bar on top
       set -g status-position top
 
-      # Sessionx plugin
-      set -g @sessionx-bind '<prefix>+O'
-      set -g @sessionx-x-path '~/nixdots'
-      set -g @sessionx-filter-current 'false'
-      set -g @sessionx-preview-enabled 'true'
-      set -g @sessionx-window-height '95%'
-      set -g @sessionx-window-width '75%'
-      set -g @sessionx-zoxide-mode 'on'
-
       # Tmux sessionizer
-      # forget the find window.  That is for chumps
       bind-key -r f run-shell "tmux neww tmux-sessionizer-script"
 
       #--------------------------------------------------------------------------
       # Status line
       #--------------------------------------------------------------------------
-      # Enable catppuccin theme
-      # set -g @catppuccin_flavour 'mocha'
+
+      # Catppuccin
+      # Set to better match the catppuccin theme
+      # set -g pane-active-border-style fg="#c6a0f6"
+
       # Enable transparent tmux bar
       # set -g status-bg default
       # set -g status-style bg=default
 
+      # TokyoNight colors for Tmux
+      set -g mode-style "fg=#7aa2f7,bg=#3b4261"
+
+      set -g message-style "fg=#7aa2f7,bg=#3b4261"
+      set -g message-command-style "fg=#7aa2f7,bg=#3b4261"
+
+      set -g pane-border-style "fg=#3b4261"
+      set -g pane-active-border-style "fg=#7aa2f7"
+
+      set -g status "on"
+      set -g status-justify "left"
+
+      set -g status-style "fg=#7aa2f7,bg=#1f2335"
+
+      set -g status-left-length "100"
+      set -g status-right-length "100"
+
+      set -g status-left-style NONE
+      set -g status-right-style NONE
+
+      set -g status-left "#[fg=#1d202f,bg=#7aa2f7,bold] #S #[fg=#7aa2f7,bg=#1f2335,nobold,nounderscore,noitalics]"
+      set -g status-right "#[fg=#1f2335,bg=#1f2335,nobold,nounderscore,noitalics]#[fg=#7aa2f7,bg=#1f2335] #{prefix_highlight} #[fg=#3b4261,bg=#1f2335,nobold,nounderscore,noitalics]#[fg=#7aa2f7,bg=#3b4261] %Y-%m-%d  %I:%M %p #[fg=#7aa2f7,bg=#3b4261,nobold,nounderscore,noitalics]#[fg=#1d202f,bg=#7aa2f7,bold] #h "
+      if-shell '[ "$(tmux show-option -gqv "clock-mode-style")" == "24" ]' {
+        set -g status-right "#[fg=#1f2335,bg=#1f2335,nobold,nounderscore,noitalics]#[fg=#7aa2f7,bg=#1f2335] #{prefix_highlight} #[fg=#3b4261,bg=#1f2335,nobold,nounderscore,noitalics]#[fg=#7aa2f7,bg=#3b4261] %Y-%m-%d  %H:%M #[fg=#7aa2f7,bg=#3b4261,nobold,nounderscore,noitalics]#[fg=#1d202f,bg=#7aa2f7,bold] #h "
+      }
+
+      setw -g window-status-activity-style "underscore,fg=#a9b1d6,bg=#1f2335"
+      setw -g window-status-separator ""
+      setw -g window-status-style "NONE,fg=#a9b1d6,bg=#1f2335"
+      setw -g window-status-format "#[fg=#1f2335,bg=#1f2335,nobold,nounderscore,noitalics]#[default] #I  #W #F #[fg=#1f2335,bg=#1f2335,nobold,nounderscore,noitalics]"
+      setw -g window-status-current-format "#[fg=#1f2335,bg=#3b4261,nobold,nounderscore,noitalics]#[fg=#7aa2f7,bg=#3b4261,bold] #I  #W #F #[fg=#3b4261,bg=#1f2335,nobold,nounderscore,noitalics]"
+
+      # tmux-plugins/tmux-prefix-highlight support
+      set -g @prefix_highlight_output_prefix "#[fg=#e0af68]#[bg=#1f2335]#[fg=#1f2335]#[bg=#e0af68]"
+      set -g @prefix_highlight_output_suffix ""
+
       # Status line customisation
-      set-option -g status-left-length 100
-      set-option -g status-right-length 100
-      set-option -g status-left " #{session_name}  "
-      set-option -g status-right " "
+      # set-option -g status-left-length 100
+      # set-option -g status-right-length 100
+      # set-option -g status-left " #{session_name}  "
+      # set-option -g status-right " "
+      #
+      # set-option -g status-style "fg=#828bb1 bg=default" # default will set the background to transparent
 
-      set-option -g status-style "fg=#828bb1 bg=default" # default will set the background to transparent
-
-      set-option -g window-status-format "#{window_index}:#{window_name}#{window_flags} " # window_name -> pane_current_command
-      set-option -g window-status-current-format "#{window_index}:#{window_name}#{window_flags} "
-      set-option -g window-status-current-style "fg=#B4BEFE"
-      set-option -g window-status-activity-style none
+      # set-option -g window-status-format "#{window_index}:#{window_name}#{window_flags} " # window_name -> pane_current_command
+      # set-option -g window-status-current-format "#{window_index}:#{window_name}#{window_flags} "
+      # set-option -g window-status-current-style "fg=#B4BEFE"
+      # set-option -g window-status-activity-style none
 
       # clock mode
       # setw -g clock-mode-colour colour1
@@ -122,8 +126,8 @@
       # setw -g mode-style "fg=colour1 bg=colour18 bold"
 
       # pane borders
-      set -g pane-border-style "fg=colour1"
-      set -g pane-active-border-style "fg=colour3"
+      # set -g pane-border-style "fg=colour1"
+      # set -g pane-active-border-style "fg=colour3"
 
       # statusbar
       # set -g status-justify left
@@ -143,42 +147,6 @@
       #
       # # messages
       # set -g message-style "fg=colour2 bg=colour0 bold"
-
-      # Biscuit
-      # set-option -g status-left-length 0
-      # set-option -g status-right-length 0
-      # set -g status-position bottom
-      # set -g status-justify centre
-      # set -g status-left "#[bg=#171717, fg=#8e8aa9]   #[bg=#171717, fg=#a38c8c]#{session_name} #[fg=#151515]█#[fg=#131313]█#[fg=#111111]█"
-      # set -g window-status-format "#[bg=#171717, fg=#736363] #I:#W "
-      # set -g window-status-current-format "#[bg=#171717, fg=#8e8aa9] #I·#W "
-      # set -g status-right "#[fg=#111111]█#[fg=#131313]█#[fg=#151515]█#[bg=#171717, fg=#a38c8c] %H:%M #[bg=#171717, fg=#8e8aa9]  "
-      # set -g window-status-activity-style none
-
-      # Tokyonight
-      # # ----- Messages -----
-      # set-option -g mode-style 'bg=blue, fg=black'
-      # set-option -g message-style 'bg=blue, fg=black'
-      #
-      # # ----- Center -----
-      # set-option -g status-justify centre
-      # set-option -g status-style "bg=black"
-      # set-window-option -g window-status-current-format '#[bold]#[fg=blue, bg=black]#[fg=black, bg=blue]#I:#W#[fg=blue, bg=black]'
-      # set-window-option -g window-status-format '#I:#W'
-      # set-window-option -g window-status-separator ' '
-      # set-window-option -g window-status-style "bg=black"
-      # set-window-option -g window-status-current-style "bg=blue,fg=black"
-
-
-      # # ----- Left -----
-      # set-option -g status-left " #S #[fg=blue, bg=black]"
-      # set-option -g status-left-style "bg=blue,fg=black"
-      #
-      #
-      # # ----- Right -----
-      # set-option -g status-right "#[fg=blue, bg=black] #[fg=black, bg=blue] %d/%m  %R "
-      # set-option -g status-right-style "bg=black,fg=blue"
-
 
       # Nord
       #+--- tmux-prefix-highlight ---+
@@ -208,4 +176,38 @@
       # set -g pane-active-border-style "bg=default fg=blue"
     '';
   };
+  home.packages = with pkgs; [
+    # https://github.com/edr3x/tmux-sessionizer?tab=readme-ov-file#tmux-sessionizer
+    tmux-sessionizer
+    # Script to find files with tmux in vim
+    (writeShellScriptBin "tmux-sessionizer-script" ''
+          if [[ $# -eq 1 ]]; then
+          selected=$1
+      else
+          selected=$(find ~/ ~/projects ~/tests -mindepth 1 -maxdepth 1 -type d | fzf)
+      fi
+
+      if [[ -z $selected ]]; then
+          exit 0
+      fi
+
+      selected_name=$(basename "$selected" | tr . _)
+      tmux_running=$(pgrep tmux)
+
+      if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
+          tmux new-session -s $selected_name -c $selected
+          exit 0
+      fi
+
+      if ! tmux has-session -t=$selected_name 2> /dev/null; then
+          tmux new-session -ds $selected_name -c $selected
+      fi
+
+      if [[ -z $TMUX ]]; then
+          tmux attach -t $selected_name
+      else
+          tmux switch-client -t $selected_name
+      fi
+    '')
+  ];
 }
