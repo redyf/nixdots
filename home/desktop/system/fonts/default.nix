cda21ab853474907cc1e643da95d34611b77f5b2
Redyf <mateusalvespereira7@gmail.com>
2024-03-13 17:39:10 -0300

    refactor: Improve config

--- home/desktop/system/fonts/default.nix
+++ home/desktop/system/fonts/default.nix
@@ -7,7 +7,6 @@
       fira-code-symbols
       (iosevka-bin.override {variant = "aile";})
       material-design-icons
-      # monolisa
       monolisa-script
       (nerdfonts.override {fonts = ["FiraMono" "JetBrainsMono"];})
       noto-fonts
