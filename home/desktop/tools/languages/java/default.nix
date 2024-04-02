8aea73211ddc0e3bc9ed00cba001493baf246b7a
Redyf <mateusalvespereira7@gmail.com>
2024-04-01 21:56:38 -0300

    feat: uninstall packages
    uninstalling because I'm going to use dev shells for my projects instead

--- home/desktop/tools/languages/java/default.nix
+++ home/desktop/tools/languages/java/default.nix
@@ -3,8 +3,8 @@
     # TODO: Disabled some of them to install with mason
     jdk # Java dev kit
     # jdt-language-server # Jdtls integration
-    checkstyle # Linter
-    google-java-format # Formatter
+    # checkstyle # Linter
+    # google-java-format # Formatter
     # vscode-extensions.vscjava.vscode-java-debug
     # vscode-extensions.vscjava.vscode-java-test
     # maven # Build automation tool for java
