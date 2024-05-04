d7f9d0455d20231c1a2eb566af7654d2a33339ae
Redyf <mateusalvespereira7@gmail.com>
2024-05-04 16:28:33 -0300

    feat: add emacs override

--- home/redyf/apps/emacs/default.nix
+++ home/redyf/apps/emacs/default.nix
@@ -1,7 +1,13 @@
 {pkgs, ...}: {
   programs.emacs = {
     enable = true;
-    package = pkgs.emacs; # replace with pkgs.emacs-gtk, or a version provided by the community overlay if desired.
+    package = pkgs.emacs.override {
+      withGTK3 = true;
+      withGTK2 = false;
+      withPgtk = true; # true on wayland
+      withNativeCompilation = true;
+      withTreeSitter = true;
+    };
   };
   services.emacs = {
     enable = true;
