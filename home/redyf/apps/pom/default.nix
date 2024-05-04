7e7d5378af064ed501981c45596296fbf39e84b5
Redyf <mateusalvespereira7@gmail.com>
2024-05-04 16:28:23 -0300

    chore: delete pom package
    Not needed anymore as my PR has been approved in nixpkgs, should be
    available soon.

--- home/redyf/apps/pom/default.nix
+++
@@ -1,7 +0,0 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    (pkgs.callPackage ../../../../pkgs/pom.nix {
-      src = pom; # pkg source is passed as a `specialArgs` and injected into this module.
-    })
-  ];
-}
