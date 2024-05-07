ace44d5abae5738dace1ed684b0c1d7e53ef3ec5
Redyf <mateusalvespereira7@gmail.com>
2024-05-06 22:51:23 -0300

    feat: add custom theme for kitty term

---
+++ home/redyf/desktop/addons/kitty/custom-mocha.nix
@@ -0,0 +1,37 @@
+''
+  background #11111B
+
+  # Cursor
+  cursor #cba6f7
+
+  # Border
+  active_border_color #78a9ff
+  inactive_border_color #535353
+  bell_border_color #3ddbd9
+
+  # Tabs
+  active_tab_background #78a9ff
+  active_tab_foreground #0c0c0c
+  inactive_tab_background #2a2a2a
+  inactive_tab_foreground #6e6f70
+
+  # normal
+  color0 #11111B
+  color1 #ff5555
+  color2 #afffd7
+  color3 #f1fa8c
+  color4 #87afff
+  color5 #bd93f9
+  color6 #8be9fd
+  color7 #f8f8f2
+
+  # bright
+  color8 #2d5b69
+  color9 #ff665c
+  color10 #84c747
+  color11 #ebc13d
+  color12 #58a3ff
+  color13 #ff84cd
+  color14 #53d6c7
+  color15 #cad8d9
+''
