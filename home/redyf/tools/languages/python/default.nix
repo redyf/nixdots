9e6dda5e12e93b34413a27dc52c2c6ed03fc2aaa
Redyf <mateusalvespereira7@gmail.com>
2024-05-12 19:52:46 -0300

    chore: remove unneeded packages

--- home/redyf/tools/languages/python/default.nix
+++ home/redyf/tools/languages/python/default.nix
@@ -1,11 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     poetry
-    (python311.withPackages (ps:
-      with ps; [
-        # black # Python formatter
-        pynvim
-        # flake8 # Linter for Python
-      ]))
+    python3
   ];
 }
