ad074c7f6847b8b7f74f394ecbc77b9d8bbb0a5b
Redyf <mateusalvespereira7@gmail.com>
2024-07-03 22:33:05 -0300

    remove: unused fonts

--- home/redyf/system/fonts/default.nix
+++ home/redyf/system/fonts/default.nix
@@ -1,17 +1,19 @@
-{pkgs, ...}: {
+{
+  inputs,
+  pkgs,
+  ...
+}: {
   home = {
     packages = with pkgs; [
       dejavu_fonts
       font-awesome
       fira-code-symbols
       material-design-icons
-      (nerdfonts.override {fonts = ["JetBrainsMono"];})
       noto-fonts
       powerline-symbols
       # monolisa-script
       # berkeley
       # sf-mono-liga-bin
-      # geist-mono
     ];
   };
 }
