94817fccc1a2f4d5bc47aef7e035c9600ca92433
redyf <mateusalvespereira7@gmail.com>
2024-07-06 19:39:22 -0300

    feat: update config for raspberry pi 5

--- hosts/selene/configuration.nix
+++ hosts/selene/configuration.nix
@@ -11,10 +11,6 @@
     firewall.enable = false;
     wireless = {
       enable = true;
-      iwd = {
-        enable = true;
-        settings.General.EnableNetworkConfiguration = true;
-      };
     };
   };
 
@@ -37,9 +33,8 @@
     extraGroups = ["networkmanager" "wheel" "input" "docker" "kvm" "libvirtd"];
   };
 
-  nix = {
-    package = pkgs.nixFlakes;
-    extraOptions = "experimental-features = nix-command flakes";
+  programs = {
+  zsh.enable = true;
   };
 
   time.timeZone = "America/Bahia";
@@ -47,17 +42,6 @@
 
   i18n = {
     defaultLocale = "pt_BR.UTF-8";
-    extraLocaleSettings = {
-      LC_ADDRESS = "pt_BR.UTF-8";
-      LC_IDENTIFICATION = "pt_BR.UTF-8";
-      LC_MEASUREMENT = "pt_BR.UTF-8";
-      LC_MONETARY = "pt_BR.UTF-8";
-      LC_NAME = "pt_BR.UTF-8";
-      LC_NUMERIC = "pt_BR.UTF-8";
-      LC_PAPER = "pt_BR.UTF-8";
-      LC_TELEPHONE = "pt_BR.UTF-8";
-      LC_TIME = "pt_BR.UTF-8";
-    };
   };
 
   # just for access after install to continue provisioning
