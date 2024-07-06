21cee637498f265c79b357d9d55601a064879fc4
redyf <mateusalvespereira7@gmail.com>
2024-07-06 20:07:16 -0300

    refactor: move part of config to selene host

--- hosts/selene/configuration.nix
+++ hosts/selene/configuration.nix
@@ -23,7 +23,31 @@
     git
     bluez
     bluez-tools
+    cowsay
   ];
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+  };
+
+  hardware = {
+    bluetooth.enable = true;
+    raspberry-pi = {
+      config = {
+        all = {
+          base-dt-params = {
+            # enable autoprobing of bluetooth driver
+            # https://github.com/raspberrypi/linux/blob/c8c99191e1419062ac8b668956d19e788865912a/arch/arm/boot/dts/overlays/README#L222-L224
+            krnbt = {
+              enable = true;
+              value = "on";
+            };
+          };
+        };
+      };
+    };
+  };
 
   users.users.selene = {
     isNormalUser = true;
