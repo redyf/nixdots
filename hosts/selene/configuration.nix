b101d9c6f438c5f1f5a719aaffcde274176c072b
Redyf <mateusalvespereira7@gmail.com>
2024-07-07 12:17:39 -0300

    fix: install failure due to non-existent options

--- hosts/selene/configuration.nix
+++ hosts/selene/configuration.nix
@@ -41,15 +41,15 @@
     bluetooth.enable = true;
     raspberry-pi = {
       config = {
-        all = {
-          options = {
-            dt-overlays = {
-              vc4-kms-v3d-pi5 = {
-                enable = true;
-                params = {};
-              };
+        pi5 = {
+          dt-overlays = {
+            vc4-kms-v3d-pi5 = {
+              enable = true;
+              params = {};
             };
           };
+        };
+        all = {
           base-dt-params = {
             # enable autoprobing of bluetooth driver
             # https://github.com/raspberrypi/linux/blob/c8c99191e1419062ac8b668956d19e788865912a/arch/arm/boot/dts/overlays/README#L222-L224
