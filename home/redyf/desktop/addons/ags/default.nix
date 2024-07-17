ce1b3276816e295bc240dfac7154cbd12adc2df3
Redyf <mateusalvespereira7@gmail.com>
2024-07-17 14:01:01 -0300

    refactor: make desktop directory modular

--- home/redyf/desktop/addons/ags/default.nix
+++ home/redyf/desktop/addons/ags/default.nix
@@ -1,33 +1,38 @@
 {
   inputs,
   pkgs,
+  lib,
+  config,
   ...
 }: {
-  # add the home manager module
   imports = [inputs.ags.homeManagerModules.default];
-
-  home.packages = with pkgs; [
-    dart-sass
-    brightnessctl
-    inputs.matugen.packages.${system}.default
-    wf-recorder
-    wayshot
-    hyprpicker
-    pavucontrol
-    pamixer
-  ];
-
-  programs.ags = {
-    enable = true;
-
-    # null or path, leave as null if you don't want hm to manage the config
-    configDir = ./config;
-
-    # additional packages to add to gjs's runtime
-    extraPackages = with pkgs; [
-      gtksourceview
-      webkitgtk
-      accountsservice
+  options = {
+    ags.enable = lib.mkEnableOption "Enable ags module";
+  };
+  config = lib.mkIf config.ags.enable {
+    home.packages = with pkgs; [
+      dart-sass
+      brightnessctl
+      inputs.matugen.packages.${system}.default
+      wf-recorder
+      wayshot
+      hyprpicker
+      pavucontrol
+      pamixer
     ];
+
+    programs.ags = {
+      enable = true;
+
+      # null or path, leave as null if you don't want hm to manage the config
+      configDir = ./config;
+
+      # additional packages to add to gjs's runtime
+      extraPackages = with pkgs; [
+        gtksourceview
+        webkitgtk
+        accountsservice
+      ];
+    };
   };
 }
