054ff1659354cba0b6bb49b9376bddf30be25e33
Redyf <mateusalvespereira7@gmail.com>
2024-07-16 22:41:40 -0300

    refactor: make config more modular (tools)

--- home/redyf/tools/http/default.nix
+++ home/redyf/tools/http/default.nix
@@ -1,3 +1,13 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [curl openssl];
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    http.enable = lib.mkEnableOption "Enable http module";
+  };
+  config = lib.mkIf config.http.enable {
+    home.packages = with pkgs; [curl openssl];
+  };
 }
