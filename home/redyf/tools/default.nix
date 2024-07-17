b7ed54293b9464f12e089fa558446ee179280e63
Redyf <mateusalvespereira7@gmail.com>
2024-07-16 22:41:40 -0300

    refactor: make config more modular (tools)

--- home/redyf/tools/default.nix
+++ home/redyf/tools/default.nix
@@ -1,8 +1,13 @@
-_: {
+{lib, ...}: {
   imports = [
-    ./direnv
     ./git
     ./http
+    ./direnv
     ./languages
   ];
+
+  git.enable = lib.mkDefault true;
+  http.enable = lib.mkDefault true;
+  direnv.enable = lib.mkDefault true;
+  languages.enable = lib.mkDefault true;
 }
--- home/redyf/tools/languages/go/default.nix
+++ home/redyf/tools/languages/go/default.nix
@@ -1,3 +1,13 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [go];
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    go.enable = lib.mkEnableOption "Enable go module";
+  };
+  config = lib.mkIf config.go.enable {
+    home.packages = with pkgs; [go];
+  };
 }
