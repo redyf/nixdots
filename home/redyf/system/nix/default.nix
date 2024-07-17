beffc936d59d1c2b359f958a00507b87577d1297
Redyf <mateusalvespereira7@gmail.com>
2024-07-16 22:40:55 -0300

    refactor: make config more modular (system)

--- home/redyf/system/nix/default.nix
+++ home/redyf/system/nix/default.nix
@@ -1,10 +1,22 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    nil
-    statix
-    alejandra
-    nix-init
-    nix-update
-    nixpkgs-review
-  ];
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    nixy.enable = lib.mkEnableOption "Enable nix module";
+  };
+  config = lib.mkIf config.nixy.enable {
+    home = {
+      packages = with pkgs; [
+        nil
+        statix
+        alejandra
+        nix-init
+        nix-update
+        nixpkgs-review
+      ];
+    };
+  };
 }
