98254f30de379dccd1eea1170338038178af6a6c
Redyf <mateusalvespereira7@gmail.com>
2024-07-17 12:29:22 -0300

    refactor: make config more modular (apps)

--- home/redyf/apps/emacs/default.nix
+++ home/redyf/apps/emacs/default.nix
@@ -1,16 +1,26 @@
-{pkgs, ...}: {
-  programs.emacs = {
-    enable = true;
-    package = pkgs.emacs.override {
-      withGTK3 = true;
-      withGTK2 = false;
-      withPgtk = true; # true on wayland
-      withNativeCompilation = true;
-      withTreeSitter = true;
-    };
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    emacs.enable = lib.mkEnableOption "Enable emacs module";
   };
-  services.emacs = {
-    enable = true;
-    package = pkgs.emacs; # replace with emacs-gtk, or a version provided by the community overlay if desired.
+  config = lib.mkIf config.emacs.enable {
+    programs.emacs = {
+      enable = true;
+      package = pkgs.emacs.override {
+        withGTK3 = true;
+        withGTK2 = false;
+        withPgtk = true; # true on wayland
+        withNativeCompilation = true;
+        withTreeSitter = true;
+      };
+    };
+    services.emacs = {
+      enable = true;
+      package = pkgs.emacs; # replace with emacs-gtk, or a version provided by the community overlay if desired.
+    };
   };
 }
