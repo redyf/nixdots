742e46ff36a264d8a47f5081573942df9919364e
Redyf <mateusalvespereira7@gmail.com>
2024-07-17 12:29:22 -0300

    refactor: make config more modular (apps)

--- home/redyf/apps/obs/default.nix
+++ home/redyf/apps/obs/default.nix
@@ -1,11 +1,21 @@
-{pkgs, ...}: {
-  programs.obs-studio = {
-    enable = true;
-    plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
-      obs-vaapi
-      obs-vkcapture
-      obs-pipewire-audio-capture
-    ];
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: {
+  options = {
+    obs.enable = lib.mkEnableOption "Enable obs module";
+  };
+  config = lib.mkIf config.obs.enable {
+    programs.obs-studio = {
+      enable = true;
+      plugins = with pkgs.obs-studio-plugins; [
+        wlrobs
+        obs-vaapi
+        obs-vkcapture
+        obs-pipewire-audio-capture
+      ];
+    };
   };
 }
