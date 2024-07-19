7c4190690a9e989381fa1459d9abae59b06d5ce0
Redyf <mateusalvespereira7@gmail.com>
2024-07-19 13:04:52 -0300

    chore: update paths for pkgs after huge refactor

--- home/apps/artix-game-launcher/default.nix
+++ home/apps/artix-game-launcher/default.nix
@@ -9,7 +9,7 @@
   };
   config = lib.mkIf config.artix-game-launcher.enable {
     home.packages = with pkgs; [
-      (pkgs.callPackage ../../../../pkgs/artixlauncher.nix {
+      (pkgs.callPackage ../../../pkgs/artixlauncher.nix {
         src = artixlauncher; # pkg source is passed as a `specialArgs` and injected into this module.
       })
     ];
