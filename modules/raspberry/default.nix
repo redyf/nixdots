37164ac2c0abea1b54e9afdcbbeba778898b9dbc
Redyf <mateusalvespereira7@gmail.com>
2024-07-18 22:29:19 -0300

    feat: add raspberry module

--- modules/raspberry/default.nix
+++ modules/raspberry/default.nix
@@ -1,16 +1,5 @@
-{
-  lib,
-  config,
-  ...
-}: {
+_: {
   imports = [
     ./raspberry-hardware.nix
   ];
-  options = {
-    raspberry.enable = lib.mkEnableOption "Enable raspberry module";
-  };
-
-  config = lib.mkIf config.raspberry.enable {
-    raspberry-hardware.enable = lib.mkDefault true;
-  };
 }
