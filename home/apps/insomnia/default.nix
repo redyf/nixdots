c2810f6867748ea41e6573ae4e1c652ccdb22cd3
Redyf <mateusalvespereira7@gmail.com>
2024-07-19 13:04:52 -0300

    chore: update paths for pkgs after huge refactor

--- home/apps/insomnia/default.nix
+++ home/apps/insomnia/default.nix
@@ -9,7 +9,7 @@
   };
   config = lib.mkIf config.insomnia.enable {
     home.packages = with pkgs; [
-      (pkgs.callPackage ../../../../pkgs/insomnia.nix {
+      (pkgs.callPackage ../../../pkgs/insomnia.nix {
         src = insomnia; # pkg source is passed as a `specialArgs` and injected into this module.
       })
     ];
