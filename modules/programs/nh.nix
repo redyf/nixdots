c8be976accb7e2bb92765dc2ac93e902ba9d0ee8
Redyf <mateusalvespereira7@gmail.com>
2024-07-18 22:51:29 -0300

    refactor: make nh module dynamic
    Now I don't need to change the path when importing this module to my
    hosts :)

--- modules/programs/nh.nix
+++ modules/programs/nh.nix
@@ -1,8 +1,10 @@
-_: {
+{homeDirectory, ...}: {
   programs.nh = {
     enable = true;
-    clean.enable = true;
-    clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/home/redyf/opensource/nixdots";
+    clean = {
+      enable = true;
+      extraArgs = "--keep-since 4d --keep 3";
+    };
+    flake = "${homeDirectory}/opensource/nixdots";
   };
 }
