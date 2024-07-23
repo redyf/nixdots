431de067babc9619e257f2d2a5e6aaf6c52961df
redyf <mateusalvespereira7@gmail.com>
2024-07-23 11:27:30 -0300

    feat: add cachix for binary cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -8,8 +8,8 @@
       warn-dirty = false;
       log-lines = 50;
       sandbox = "relaxed";
-      substituters = ["https://hyprland.cachix.org"];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+      substituters = ["https://hyprland.cachix.org" "https://nix-community.cachix.org"];
+      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
     };
     gc = {
       automatic = false;
