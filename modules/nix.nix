81a73626206c7be81f4bb99cdee8fc80f975a543
redyf <mateusalvespereira7@gmail.com>
2024-07-23 12:24:07 -0300

    feat: add raspberry-pi-nix binary cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -8,8 +8,16 @@
       warn-dirty = false;
       log-lines = 50;
       sandbox = "relaxed";
-      substituters = ["https://hyprland.cachix.org" "https://nix-community.cachix.org"];
-      trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+      substituters = [
+        "https://hyprland.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://raspberry-pi-nix.cachix.org"
+      ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "raspberry-pi-nix.cachix.org-1:WmV2rdSangxW0rZjY/tBvBDSaNFQ3DyEQsVw8EvHn9o="
+      ];
     };
     gc = {
       automatic = false;
