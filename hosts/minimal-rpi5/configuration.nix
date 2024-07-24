6d83146f18eb0e0a239921ce42bee6c7557aa0fb
redyf <mateusalvespereira7@gmail.com>
2024-07-23 21:21:45 -0300

    feat: add minimal-rpi5 host

---
+++ hosts/minimal-rpi5/configuration.nix
@@ -0,0 +1,54 @@
+{
+  pkgs,
+  username,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    ../../modules/raspberry
+    ../../modules/system/environment.nix
+    ../../modules/system/keymap.nix
+    ../../modules/system/locale.nix
+    ../../modules/system/networking.nix
+    ../../modules/nix.nix
+    ../../modules/system/security.nix
+    ../../modules/system/systemd.nix
+    ../../modules/system/time.nix
+    ../../modules/system/xdg-portal.nix
+    ../../modules/system/zram.nix
+  ];
+
+  users.users = {
+    ${username} = {
+      isNormalUser = true;
+      description = username;
+      initialPassword = "123456";
+      shell = pkgs.zsh;
+      extraGroups = ["networkmanager" "wheel" "input" "docker" "kvm" "libvirtd"];
+    };
+  };
+
+  programs = {
+    sway.enable = true;
+  };
+
+  services = {
+    openssh = {
+      enable = true;
+      settings.PermitRootLogin = "yes";
+    };
+    xserver = {
+      enable = true;
+      displayManager.gdm.enable = true;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    git
+    bluez
+    bluez-tools
+  ];
+
+  # just for access after install to continue provisioning
+  system.stateVersion = "23.11";
+}
