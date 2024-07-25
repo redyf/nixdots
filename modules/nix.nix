ec2a98b51134c41ca358a2d1a04a43228311260f
redyf <mateusalvespereira7@gmail.com>
2024-07-25 11:59:54 -0300

    fix: binary cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -4,6 +4,11 @@
   homeDirectory,
   ...
 }: {
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
   nix = {
     package = pkgs.nixFlakes;
     extraOptions = "experimental-features = nix-command flakes";
@@ -14,39 +19,31 @@
       log-lines = 50;
       sandbox = "relaxed";
       substituters = [
-        "https://hyprland.cachix.org"
         "https://nix-community.cachix.org"
-        "https://raspberry-pi-nix.cachix.org"
+        "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "raspberry-pi-nix.cachix.org-1:WmV2rdSangxW0rZjY/tBvBDSaNFQ3DyEQsVw8EvHn9o="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
-      trusted-users = ["${username}"];
     };
     gc = {
       automatic = false;
       dates = "weekly";
       options = "--delete-older-than 7d";
     };
-    distributedBuilds = true;
-    buildMachines = [
-      {
-        hostName = "raspberry";
-        sshUser = "redyf";
-        sshKey = "${homeDirectory}/.ssh/id_ed25519_buildmachine";
-        systems = ["aarch64-linux"];
-        protocol = "ssh-ng";
-        maxJobs = 1;
-        speedFactor = 1;
-        supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
-      }
-    ];
-  };
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-    };
+    # distributedBuilds = true;
+    # buildMachines = [
+    #   {
+    #     hostName = "raspberry";
+    #     sshUser = "redyf";
+    #     sshKey = "${homeDirectory}/.ssh/id_ed25519_buildmachine";
+    #     systems = ["aarch64-linux"];
+    #     protocol = "ssh-ng";
+    #     maxJobs = 1;
+    #     speedFactor = 1;
+    #     supportedFeatures = ["nixos-test" "benchmark" "big-parallel" "kvm"];
+    #   }
+    # ];
   };
 }
