c94f6169276d5899f265989232cfd04f70650ad2
redyf <mateusalvespereira7@gmail.com>
2024-07-25 11:59:54 -0300

    fix: binary cache

--- home/system/nix/default.nix
+++ home/system/nix/default.nix
@@ -19,19 +19,5 @@
         cachix
       ];
     };
-  nix = {
-    settings = {
-      substituters = [
-        "https://hyprland.cachix.org"
-        "https://nix-community.cachix.org"
-        "https://raspberry-pi-nix.cachix.org"
-      ];
-      trusted-public-keys = [
-        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "raspberry-pi-nix.cachix.org-1:WmV2rdSangxW0rZjY/tBvBDSaNFQ3DyEQsVw8EvHn9o="
-      ];
-    };
   };
-    };
 }
