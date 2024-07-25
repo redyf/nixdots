b18e8f20fe1684e159f862b07d148154533252a2
redyf <mateusalvespereira7@gmail.com>
2024-07-25 11:59:20 -0300

    remove: rpi flake

--- flake.nix
+++ flake.nix
@@ -8,10 +8,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    nix-rpi5 = {
-      url = "gitlab:vriska/nix-rpi5";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # Overlays
     raspberry-pi-nix.url = "github:tstat/raspberry-pi-nix";
 
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
