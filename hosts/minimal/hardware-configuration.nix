ce860b689b6a99f038533564fe9a54c849d2df6a
redyf <mateusalvespereira7@gmail.com>
2024-07-25 11:59:20 -0300

    remove: rpi flake

--- hosts/minimal/hardware-configuration.nix
+++ hosts/minimal/hardware-configuration.nix
@@ -2,7 +2,6 @@
 # and may be overwritten by future invocations.  Please make changes
 # to /etc/nixos/configuration.nix instead.
 {
-  inputs,
   config,
   lib,
   pkgs,
@@ -16,8 +15,6 @@
   boot.initrd.availableKernelModules = ["usb_storage"];
   boot.initrd.kernelModules = [];
   boot.kernelModules = [];
-  boot.kernelPackages = lib.mkForce inputs.nix-rpi5.legacyPackages.aarch64-linux.linuxPackages_rpi5;
-  # boot.kernelPackages = (import (builtins.fetchTarball https://gitlab.com/vriska/nix-rpi5/-/archive/main.tar.gz)).legacyPackages.aarch64-linux.linuxPackages_rpi5;
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
