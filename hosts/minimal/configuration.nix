70a4314dc5bb951628317e74903b33111a0e4e90
redyf <mateusalvespereira7@gmail.com>
2024-07-30 23:25:57 -0300

    feat: add hyprland substituter

--- hosts/minimal/configuration.nix
+++ hosts/minimal/configuration.nix
@@ -84,9 +84,11 @@
       log-lines = 50;
       substituters = [
         "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       ];
     };
     gc = {
