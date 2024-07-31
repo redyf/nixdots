1c6c6e4a781932d53176a804c5ca4f495bb3549a
redyf <mateusalvespereira7@gmail.com>
2024-07-30 21:45:33 -0300

    feat: add new sets for minimal-rpi5 host

--- hosts/minimal/configuration.nix
+++ hosts/minimal/configuration.nix
@@ -4,21 +4,45 @@
   ...
 }: {
   time.timeZone = "America/Bahia";
+  console = {keyMap = "br-abnt2";};
+  i18n = {
+    defaultLocale = "pt_BR.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "pt_BR.UTF-8";
+      LC_IDENTIFICATION = "pt_BR.UTF-8";
+      LC_MEASUREMENT = "pt_BR.UTF-8";
+      LC_MONETARY = "pt_BR.UTF-8";
+      LC_NAME = "pt_BR.UTF-8";
+      LC_NUMERIC = "pt_BR.UTF-8";
+      LC_PAPER = "pt_BR.UTF-8";
+      LC_TELEPHONE = "pt_BR.UTF-8";
+      LC_TIME = "pt_BR.UTF-8";
+    };
+  };
+
   users.users.root.initialPassword = "root";
   networking = {
     hostName = "minimal";
+    networkmanager.enable = true;
     useDHCP = false;
     interfaces = {
       wlan0.useDHCP = true;
       eth0.useDHCP = true;
     };
   };
+
+  environment.systemPackages = with pkgs; [
+    git
+    neovim
+  ];
+
   # Overlays for raspberry-pi
   raspberry-pi-nix = {
     board = "bcm2712"; # bcm2711 if on rpi4 or below
     uboot.enable = false;
     libcamera-overlay.enable = false;
   };
+
   hardware = {
     bluetooth.enable = true;
     raspberry-pi = {
@@ -42,6 +66,26 @@
           };
         };
       };
+    };
+  };
+
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = "experimental-features = nix-command flakes";
+    settings = {
+      auto-optimise-store = true;
+      log-lines = 50;
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
+    };
+    gc = {
+      automatic = false;
+      dates = "weekly";
+      options = "--delete-older-than 7d";
     };
   };
 }
