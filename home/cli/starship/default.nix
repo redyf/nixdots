7814ab27d9b49c158235daaa70992e185dd48cd6
redyf <mateusalvespereira7@gmail.com>
2024-08-05 16:07:35 -0300

    feat: add new alternative theme for oxocarbon

--- home/cli/starship/default.nix
+++ home/cli/starship/default.nix
@@ -3,21 +3,24 @@
   lib,
   config,
   ...
-}: {
+}:
+{
   options = {
     starship.enable = lib.mkEnableOption "Enable starship module";
   };
   config = lib.mkIf config.starship.enable {
-    programs.starship = let
-      mochaPreset = import ./mocha.nix;
-      oxocarbonPreset = import ./oxocarbon.nix;
-      bloatedPreset = import ./bloated.nix;
-      macchiatoPreset = import ./macchiato.nix {inherit pkgs;};
-      pastelPreset = builtins.fromTOML (builtins.readFile ./pastel.toml);
-    in {
-      enable = true;
-      enableZshIntegration = true;
-      settings = oxocarbonPreset;
-    };
+    programs.starship =
+      let
+        mochaPreset = import ./mocha.nix;
+        oxocarbonPreset = import ./oxocarbon.nix;
+        bloatedPreset = import ./bloated.nix;
+        macchiatoPreset = import ./macchiato.nix { inherit pkgs; };
+        oxocarbonv2 = builtins.fromTOML (builtins.readFile ./oxocarbonv2.toml);
+      in
+      {
+        enable = true;
+        enableZshIntegration = true;
+        settings = oxocarbonPreset;
+      };
   };
 }
