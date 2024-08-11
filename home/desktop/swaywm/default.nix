33be943fea9ef0f83b59b36e0000383b7667b66c
redyf <mateusalvespereira7@gmail.com>
2024-08-11 20:50:46 -0300

    feat: add swaywm module

---
+++ home/desktop/swaywm/default.nix
@@ -0,0 +1,22 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+  options = {
+    swaywm.enable = lib.mkEnableOption "Enable sway module";
+  };
+  config = lib.mkIf config.swaywm.enable {
+    wayland.windowManager.sway = {
+      enable = true;
+      config = rec {
+        terminal = "ghostty";
+        # startup = [
+        #
+        # ];
+      };
+    };
+  };
+}
