ae56fcc86dfe3c39fbf4625956c6926d30d7b3b5
redyf <mateusalvespereira7@gmail.com>
2024-08-21 23:01:50 -0300

    feat: add new colorschemes to stylix

--- modules/stylix.nix
+++ modules/stylix.nix
@@ -1,7 +1,29 @@
 { inputs, pkgs, ... }:
 let
   themes = {
+    catppuccin-mocha = "catppuccin-mocha";
+    mountain = "mountain";
     oxocarbon-dark = "oxocarbon-dark";
+    jabuti = {
+      scheme = "Jabuti";
+      author = "Notusknot";
+      base00 = "292a37";
+      base01 = "343545";
+      base02 = "3c3e51";
+      base03 = "45475d";
+      base04 = "50526b";
+      base05 = "c0cbe3";
+      base06 = "d9e0ee";
+      base07 = "ffffff";
+      base08 = "ec6a88";
+      base09 = "efb993";
+      base0A = "e1c697";
+      base0B = "3fdaa4";
+      base0C = "ff7eb6";
+      base0D = "3fc6de";
+      base0E = "be95ff";
+      base0F = "8b8da9";
+    };
   };
 in
 {
@@ -9,13 +31,12 @@ in
     enable = true;
     autoEnable = true;
     image = ../hosts/redyf/9ovcXG0Wo4P7FQPe.jpg;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/${themes.oxocarbon-dark}.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/${themes.mountain}.yaml";
+    # base16Scheme = themes.jabuti;
     fonts = {
       monospace = {
-        # package = inputs.font-flake.packages.${pkgs.system}.monolisa;
-        # name = "Monolisa";
-        package = with pkgs; (nerdfonts.override { fonts = [ "UbuntuMono" ]; });
-        name = "UbuntuMono Nerd Font";
+        package = inputs.font-flake.packages.${pkgs.system}.berkeley;
+        name = "Liga Berkeley Mono";
       };
       sansSerif = {
         package = pkgs.dejavu_fonts;
@@ -27,7 +48,7 @@ in
       };
       sizes = {
         applications = 11;
-        terminal = 16;
+        terminal = 12;
         desktop = 11;
         popups = 11;
       };
