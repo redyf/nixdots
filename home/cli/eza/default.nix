3f30c9de1d6aa491551a4b5e56b0a3f6a92ca47e
redyf <mateusalvespereira7@gmail.com>
2024-12-05 21:15:48 -0300

    fix: starship errors

--- home/cli/eza/default.nix
+++ home/cli/eza/default.nix
@@ -11,6 +11,12 @@
     programs.eza = {
       enable = true;
       icons = "auto";
+      colors = "always";
+      extraOptions = [
+        "--no-filesize"
+        "--long"
+        "--no-user"
+      ];
       enableZshIntegration = true;
     };
   };
