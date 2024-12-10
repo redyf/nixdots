0c9b783ce33a6907ac9c15dc0e7c809ba58ba4e5
redyf <mateusalvespereira7@gmail.com>
2024-12-10 20:08:28 -0300

    chore: update flake inputs

--- home/desktop/addons/swww/default.nix
+++ home/desktop/addons/swww/default.nix
@@ -3,13 +3,14 @@
   lib,
   config,
   ...
-}: {
+}:
+{
   options = {
     swww.enable = lib.mkEnableOption "Enable swww module";
   };
   config = lib.mkIf config.swww.enable {
     home.packages = with pkgs; [
-      (pkgs.writeShellScriptBin "wallpaper" ''
+      (writeShellScriptBin "wallpaper" ''
         /usr/bin/env ls ~/wallpapers/ | sort -R | tail -1 |while read file; do
             swww img ~/wallpapers/$file --transition-fps 60 --transition-type wipe
             echo "~/wallpapers/$file"
