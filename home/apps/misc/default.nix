44cd157159e59adca03cfb89113b68c2a9f79b8c
redyf <mateusalvespereira7@gmail.com>
2025-01-02 11:23:27 -0300

    refactor: move obsidian ollama script to its own package

--- home/apps/misc/default.nix
+++ home/apps/misc/default.nix
@@ -23,36 +23,7 @@
       libreoffice
       realvnc-vnc-viewer
       postman
-      (writeShellScriptBin "obsidianOllama" ''
-        #!/usr/bin/env sh
-        start_ollama_model() {
-            echo "Iniciando o modelo do Ollama..."
-            OLLAMA_ORIGINS=app://obsidian.md* ollama serve &
-            OLLAMA_PID=$!  # Salva o PID do processo do Ollama
-        }
-
-        # Função para parar o modelo do Ollama
-        stop_ollama_model() {
-            if [[ -n "$OLLAMA_PID" ]] && kill -0 "$OLLAMA_PID" 2>/dev/null; then
-                echo "Parando o modelo do Ollama..."
-                kill "$OLLAMA_PID"
-            fi
-        }
-
-        # Inicia o modelo do Ollama
-        start_ollama_model
-
-        # Inicia o Obsidian e aguarda ele fechar
-        obsidian &
-        OBSIDIAN_PID=$!
-        wait "$OBSIDIAN_PID"
-
-        # Para o modelo do Ollama após o fechamento do Obsidian
-        stop_ollama_model
-      '')
-      obsidian
       tldr
-      ollama
 
       # Extra utils
       wget
