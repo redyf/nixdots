1fddb4a954782c8d8b1fbfbc5d236c6fc60b7b75
redyf <mateusalvespereira7@gmail.com>
2025-01-03 12:47:16 -0300

    feat: add autocommit script for obsidian

--- home/apps/obsidian/default.nix
+++ home/apps/obsidian/default.nix
@@ -4,6 +4,50 @@
   pkgs,
   ...
 }:
+let
+  gitSyncObsidian = pkgs.writeShellScriptBin "git-sync-obsidian" ''
+    #!/usr/bin/env sh
+
+    VAULT_DIR="$HOME/Documentos/GitHub_Vault"
+
+    cd $VAULT_DIR || exit 1
+
+    git add .
+
+    git commit -m "$(date '+%Y-%m-%d %H:%M:%S')" || exit 0
+
+    git pull --rebase origin main || exit 1
+    git push origin main
+  '';
+
+  obsidianOllama = pkgs.writeShellScriptBin "obsidianOllama" ''
+    #!/usr/bin/env sh
+    start_ollama_model() {
+        echo "Iniciando o modelo do Ollama..."
+        OLLAMA_ORIGINS=app://obsidian.md* ollama serve &
+        OLLAMA_PID=$!  # Salva o PID do processo do Ollama
+    }
+
+    # Função para parar o modelo do Ollama
+    stop_ollama_model() {
+        if [[ -n "$OLLAMA_PID" ]] && kill -0 "$OLLAMA_PID" 2>/dev/null; then
+            echo "Parando o modelo do Ollama..."
+            kill "$OLLAMA_PID"
+        fi
+    }
+
+    # Inicia o modelo do Ollama
+    start_ollama_model
+
+    # Inicia o Obsidian e aguarda ele fechar
+    obsidian &
+    OBSIDIAN_PID=$!
+    wait "$OBSIDIAN_PID"
+
+    # Para o modelo do Ollama após o fechamento do Obsidian
+    stop_ollama_model
+  '';
+in
 {
   options = {
     obsidian.enable = lib.mkEnableOption "Enable obsidian module";
@@ -11,34 +55,26 @@
   config = lib.mkIf config.obsidian.enable {
     home.packages = with pkgs; [
       obsidian
+      gitSyncObsidian
       # ollama
-      # (writeShellScriptBin "obsidianOllama" ''
-      #   #!/usr/bin/env sh
-      #   start_ollama_model() {
-      #       echo "Iniciando o modelo do Ollama..."
-      #       OLLAMA_ORIGINS=app://obsidian.md* ollama serve &
-      #       OLLAMA_PID=$!  # Salva o PID do processo do Ollama
-      #   }
-      #
-      #   # Função para parar o modelo do Ollama
-      #   stop_ollama_model() {
-      #       if [[ -n "$OLLAMA_PID" ]] && kill -0 "$OLLAMA_PID" 2>/dev/null; then
-      #           echo "Parando o modelo do Ollama..."
-      #           kill "$OLLAMA_PID"
-      #       fi
-      #   }
-      #
-      #   # Inicia o modelo do Ollama
-      #   start_ollama_model
-      #
-      #   # Inicia o Obsidian e aguarda ele fechar
-      #   obsidian &
-      #   OBSIDIAN_PID=$!
-      #   wait "$OBSIDIAN_PID"
-      #
-      #   # Para o modelo do Ollama após o fechamento do Obsidian
-      #   stop_ollama_model
-      # '')
+      # obsidianOllama
     ];
+    systemd.user = {
+      services.git-sync-obsidian = {
+        Unit = {
+          Description = "Sync Obsidian Vault with GitHub";
+          Wants = "git-sync-obsidian.timer";
+        };
+        Service = {
+          ExecStart = "${gitSyncObsidian}/bin/git-sync-obsidian";
+          Type = "simple";
+        };
+      };
+      timers.git-sync-obsidian = {
+        Unit.Description = "Run Git Sync for Obsidian Vault";
+        Timer.OnCalendar = "*:0/15";
+        Install.WantedBy = [ "timers.target" ];
+      };
+    };
   };
 }
