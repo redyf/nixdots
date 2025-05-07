b5d27dd411fef444b5345816825daddc49da35f8
redyf <mateusalvespereira7@gmail.com>
2025-05-06 22:09:32 -0300

    feat: add nixpkgs & garnix cache

--- modules/nix.nix
+++ modules/nix.nix
@@ -29,12 +29,16 @@
         "sonja"
       ];
       substituters = [
+        "https://cache.garnix.io"
         "https://nix-community.cachix.org"
         "https://hyprland.cachix.org"
+        "https://cache.nixos.org"
       ];
       trusted-public-keys = [
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
     };
     gc = {
