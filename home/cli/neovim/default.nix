949453bb17d0dd96ba50b06d35f2541a7ce21c3a
redyf <mateusalvespereira7@gmail.com>
2025-11-09 16:02:22 -0300

    remove: unnecessary packages for neovim

--- home/cli/neovim/default.nix
+++ home/cli/neovim/default.nix
@@ -12,19 +12,6 @@
     home = {
       packages = with pkgs; [
         neovim
-        pkgs.vimPlugins.nvim-treesitter.withAllGrammars
-        # (vimPlugins.nvim-treesitter.withPlugins (
-        #   p: with p; [
-        #     tree-sitter-query # Prioridade máxima
-        #     tree-sitter-r # Para o arquivo que você compartilhou
-        #     tree-sitter-rust # Para o arquivo que você compartilhou
-        #     tree-sitter-nix # Para editar arquivos Nix
-        #     tree-sitter-lua # Para configurações do Neovim
-        #     tree-sitter-python # Opcional, mas útil
-        #     tree-sitter-c # Opcional
-        #     tree-sitter-cpp # Opcional
-        #   ]
-        # ))
       ];
     };
   };
