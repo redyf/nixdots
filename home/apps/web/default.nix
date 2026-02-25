b716d2bd6ddf69cec19720a57e83011593defe31
redyf <mateusalvespereira7@gmail.com>
2026-02-25 11:15:55 -0300

    fix: slack login

--- home/apps/web/default.nix
+++ home/apps/web/default.nix
@@ -11,6 +11,11 @@
   config = lib.mkIf config.web.enable {
     home.packages = with pkgs; [
       vesktop
+      slack
+      # (pkgs.writeShellScriptBin "slack-x11" ''
+      #   exec env ELECTRON_OZONE_PLATFORM_HINT=x11 ${pkgs.slack}/bin/slack "$@"
+      # '')
+      # slack-x11
     ];
   };
 }
