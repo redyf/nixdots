46986b83977cff226f2022707f94cf58c1c8946b
redyf <mateusalvespereira7@gmail.com>
2026-06-09 21:30:49 -0300

    ci: add public font fallback

---
+++ ci/font-flake-fallback/flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "CI fallback for private font-flake packages";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+        in
+        {
+          tx02 = pkgs.nerd-fonts.jetbrains-mono;
+        }
+      );
+    };
+}
